231d431075353803efa528e4b17399979dbeba40
Wenaho Dong <wenaho.dong@noaa.gov>
2019-05-13 11:43:21 -0400

    Contents from tarball hosted on NCAR website
    - For the transition of maintenance and development from NCAR
      to GFDL, this initial commit contains the contents from the
      following tarball as of 13-May-2019
    
      ftp://ftp.cgd.ucar.edu/archive/mdtf/MDTF_v2.0.var_code.tar

---
+++ var_code/EOF_500hPa/settings.py
@@ -0,0 +1,12 @@
+# This file is part of the EOF_500hPa module of the MDTF code package (see mdtf/MDTF_v2.0/LICENSE.txt)
+
+# ==================================================================================================
+# 1.  EOF of geopotential height anomalies of 500 hPa
+#     This requires two files: 1) monthly averaged surface pressure (ps), 2) monthly averaged geopotential height (zg)
+#     the code is in NCL
+# ==================================================================================================
+
+driver    "EOF_500hPa.py"
+long_name "EOF of geopotential height anomalies of 500 hPa"
+
+

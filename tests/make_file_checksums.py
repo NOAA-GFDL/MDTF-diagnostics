ffd68c60492009945836d04768a9305d7567551d
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-08-06 16:39:35 -0400

    Use 'identify' for image checksums
    Change format of reference files. Use the 'identify' command-line tool
    (part of ImageMagick) to checksum image files that may have creation
    dates in the header.

--- tests/make_file_checksums.py
+++ tests/make_file_checksums.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import argparse
+import textwrap
 if os.name == 'posix' and sys.version_info[0] < 3:
     try:
         import subprocess32 as subprocess
@@ -14,53 +15,103 @@ cwd = os.path.dirname(os.path.realpath(__file__)) # gets dir of currently execut
 sys.path.insert(0, os.path.realpath(os.path.join(cwd, '..', 'var_code')))
 import util
 
-parser = argparse.ArgumentParser()
-parser.add_argument('config_file', nargs='?', type=str, 
-                    default=os.path.realpath(os.path.join(cwd, '..','config.yml')),
-                    help="Configuration file.")
-args = parser.parse_args()
+def checksum_function(file_path):
+    IMAGE_FILES = ['.eps','.ps','.png','.gif','.jpg','.jpeg']
 
-try:
-    config = util.read_mdtf_config_file(args.config_file)
-except Exception as error:
-    print error
-    exit()
-util.set_mdtf_env_vars(args, config)
-util.check_required_dirs(
-   already_exist =["MODEL_ROOT_DIR","OBS_ROOT_DIR"], 
-   create_if_nec = ["OUTPUT_DIR"])
+    print os.path.split(file_path)[1]
+    ext = os.path.splitext(file_path)[1]
+    if ext in IMAGE_FILES:
+        # use ImageMagick 'identify' command which ignores file creation time 
+        # metadata in image header file and only hashes actual image data.
+        # See https://stackoverflow.com/a/41706704
+        checksum = subprocess.check_output('identify -format "%#" '+file_path, shell=True)
+        checksum = checksum.split('\n')[0]
+    else:
+        # fallback method: system md5
+        checksum = subprocess.check_output('md5sum '+file_path, shell=True)
+        checksum = checksum.split(' ')[0]
+    return checksum
 
-md5_path = os.path.join(os.environ['DIAG_HOME'],'tests','md5')
+def checksum_files_in_subtree(dir, exclude_exts=[]):
+    start_cwd = os.getcwd()
+    checksum_dict = {}
+    os.chdir(dir)
+    # -not -path excludes hidden files from listing
+    files = subprocess.check_output('find . -type f -not -path "*/\.*"', shell=True)
+    files = files.split('\n')
+    # f[2:] removes the "./" at the beginning of each entry
+    files = [f[2:] for f in files if 
+        f != '' and (os.path.splitext(f)[1] not in exclude_exts)]
+    for f in files:
+        checksum_dict[f] = checksum_function(os.path.join(dir, f))
+    os.chdir(start_cwd)
+    return checksum_dict
 
-print 'Hashing input observational data'
-os.chdir(os.environ['OBS_ROOT_DIR'])
-subprocess.call(
-    "find . -type f -not -path '*/\.*' -print0 | xargs -0 md5sum > " \
-        + os.path.join(md5_path,'obs_data.md5'),
-    shell = True
-)
+def checksum_in_subtree_1(rootdir, exclude_exts=[]):
+    # descend into subdirectories, then flatten file hierarchy
+    checksum_dict = {}
+    dirs1 = next(os.walk(rootdir))[1]
+    for d1 in dirs1:
+        checksum_dict[d1] = checksum_files_in_subtree(os.path.join(rootdir, d1), exclude_exts)
+    return checksum_dict
 
-print 'Hashing input model data'
-os.chdir(os.environ['MODEL_ROOT_DIR'])
-subprocess.call(
-    "find . -type f -not -path '*/\.*' -print0 | xargs -0 md5sum > " \
-        + os.path.join(md5_path,'model.md5'),
-    shell = True
-)
+def checksum_in_subtree_2(rootdir, exclude_exts=[]):
+    # same except we descend 2 levels deep and then flatten
+    checksum_dict = {}
+    dirs1 = next(os.walk(rootdir))[1]
+    for d1 in dirs1:
+        checksum_dict[d1] = {}
+        dirs2 = next(os.walk(os.path.join(rootdir, d1)))[1]
+        for d2 in dirs2:
+            checksum_dict[d1][d2] = checksum_files_in_subtree(
+                os.path.join(rootdir, d1, d2), exclude_exts)
+    return checksum_dict
 
-print 'Hashing output netCDF data'
-os.chdir(os.environ['OUTPUT_DIR'])
-subprocess.call(
-    "find . -type f -name '*.nc' -print0 | xargs -0 md5sum > " \
-        + os.path.join(md5_path,'output_nc.md5'),
-    shell = True
-)
+# wrap these so we can import them in the test functions
+def make_input_data_dict(path):
+    return checksum_in_subtree_1(path)
 
-print 'Hashing output figures'
-ext = config['settings']['convert_output_fmt']
-os.chdir(os.environ['OUTPUT_DIR'])
-subprocess.call(
-    "find . -type f -name '*." + ext + "' -print0 | xargs -0 md5sum > " \
-        + os.path.join(md5_path,'output_'+ext+'.md5'),
-    shell = True
-)
+def make_output_data_dict(path):
+    return checksum_in_subtree_2(path, ['.tar','.tar_old','.log','.yml'])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file', nargs='?', type=str, 
+                        default=os.path.realpath(os.path.join(cwd, '..','config.yml')),
+                        help="Configuration file.")
+    args = parser.parse_args()
+
+    try:
+        config = util.read_mdtf_config_file(args.config_file)
+    except Exception as error:
+        print error
+        exit()
+    util.set_mdtf_env_vars(args, config)
+    util.check_required_dirs(
+    already_exist =["MODEL_ROOT_DIR","OBS_ROOT_DIR"], 
+    create_if_nec = ["OUTPUT_DIR"])
+
+    md5_path = os.path.join(os.environ['DIAG_HOME'],'tests','checksums')
+    header = """
+        # This file was produced by make_file_checksums.py and is used by the
+        # test_*_checksums.py unit tests. Don't modify it by hand!
+        #
+        """
+
+    print 'Hashing input observational data'
+    checksum_dict = make_input_data_dict(os.environ['OBS_ROOT_DIR'])
+    with open(os.path.join(md5_path, 'checksum_obs_data.yml'), 'w') as file_obj:
+        file_obj.write(textwrap.dedent(header))
+        yaml.dump(checksum_dict, file_obj)
+
+    print 'Hashing input model data'
+    checksum_dict = make_input_data_dict(os.environ['MODEL_ROOT_DIR'])
+    with open(os.path.join(md5_path, 'checksum_model_data.yml'), 'w') as file_obj:
+        file_obj.write(textwrap.dedent(header))
+        yaml.dump(checksum_dict, file_obj)
+
+    print 'Hashing output data'
+    checksum_dict = make_output_data_dict(os.environ['OUTPUT_DIR'])
+    with open(os.path.join(md5_path, 'checksum_output.yml'), 'w') as file_obj:
+        file_obj.write(textwrap.dedent(header))
+        yaml.dump(checksum_dict, file_obj)
--- tests/test_input_checksums.py
+++ tests/test_input_checksums.py
@@ -9,6 +9,7 @@ if os.name == 'posix' and sys.version_info[0] < 3:
     else:
         import subprocess
 import yaml
+from make_file_checksums import checksum_files_in_subtree
 
 DOING_TRAVIS = (os.environ.get('TRAVIS', False) == 'true')
 DOING_MDTF_DATA_TESTS = ('--data_tests' in sys.argv)
@@ -22,19 +23,20 @@ DOING_SETUP = DOING_MDTF_DATA_TESTS and not DOING_TRAVIS
 if DOING_SETUP:
     # configure paths from config.yml; currently no option to override this
     cwd = os.path.dirname(os.path.realpath(__file__)) # gets dir of currently executing script
-    md5_path = os.path.join(cwd,'md5')
+    md5_path = os.path.join(cwd,'checksums')
     with open(os.path.join(cwd,'..','config.yml'), 'r') as file_object:
         config = yaml.safe_load(file_object)
     obs_path = os.path.realpath(config['paths']['OBS_ROOT_DIR'])
     model_path = os.path.realpath(config['paths']['MODEL_ROOT_DIR'])
 
     # find PODs and model data that are present on current system
-    pods = next(os.walk('var_code'))[1]
-    pods.remove('html')
-    pods.remove('util')
-    if ('MDTF_diagnostics.egg-info') in pods:
-        pods.remove('MDTF_diagnostics.egg-info')
+    pods = next(os.walk(obs_path))[1]
     models = next(os.walk(model_path))[1]
+
+    with open(os.path.join(md5_path, 'checksum_obs_data.yml'), 'r') as file_obj:
+        obs_data_checksums = yaml.safe_load(file_obj)
+    with open(os.path.join(md5_path, 'checksum_model_data.yml'), 'r') as file_obj:
+        model_data_checksums = yaml.safe_load(file_obj)
 
 # Python 3 has subTest; in 2.7 to avoid introducing other dependencies we use
 # the advanced construction presented in https://stackoverflow.com/a/20870875 
@@ -42,32 +44,34 @@ if DOING_SETUP:
 
 class TestSequenceMeta(type):
     def __new__(mcs, name, bases, test_dict):
-        def generate_test(pod_name, md5_name, path):
+        def generate_test(name, path, reference_dict):
             def test(self):
-                os.chdir(path)
-                self.assertEqual(0, subprocess.check_call(
-                    "grep '" + pod_name + "' " \
-                        + os.path.join(md5_path, md5_name+'.md5') \
-                        + " | md5sum -c --quiet",
-                    shell=True
-                ))
+                self.assertIn(name, reference_dict)
+                if name not in reference_dict: 
+                    self.skipTest('Skipping rest of test: {} not found.'.format(name))
+                test_dict = checksum_files_in_subtree(os.path.join(path, name))
+                for key in reference_dict[name]:
+                    self.assertIn(key, test_dict,
+                        'Failure: {} not found in {}'.format(key, os.path.join(path, name)))
+                    self.assertEqual(test_dict[key], reference_dict[name][key],
+                        'Failure: Hash of {} differs from reference.'.format(os.path.join(path, name, key)))
             return test       
 
         if DOING_SETUP:
             for pod in pods:
-                test_name = "test_input_md5_%s" % pod
-                test_dict[test_name] = generate_test(pod, 'obs_data', obs_path)
+                test_name = "test_input_checksum_"+pod
+                test_dict[test_name] = generate_test(pod, obs_path, obs_data_checksums)
 
             for model in models:
-                test_name = "test_input_md5_%s" % model
-                test_dict[test_name] = generate_test(model, 'model', model_path)
+                test_name = "test_input_checksum_"+model
+                test_dict[test_name] = generate_test(model, model_path, model_data_checksums)
         return type.__new__(mcs, name, bases, test_dict)
 
 @unittest.skipIf(DOING_TRAVIS,
     "Skipping input file md5 tests because running in Travis CI environment")
 @unittest.skipUnless(DOING_MDTF_DATA_TESTS,
     "Skipping input file md5 tests because not running data-intensive test suite.")
-class TestInputMD5(unittest.TestCase):
+class TestInputChecksums(unittest.TestCase):
     __metaclass__ = TestSequenceMeta
 
 if __name__ == '__main__':

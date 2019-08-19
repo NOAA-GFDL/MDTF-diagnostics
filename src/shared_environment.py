f4b2e05300a3cb3f04c7d21e1544937577bcc450
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-08-19 10:32:42 -0400

    Remove conda-specific environment code
    Make EnvironmentManager an abstract base class, with details of
    switching environments left to child classes. Implement one child class
    for conda and one child class which doesn't do management.

--- src/shared_environment.py
+++ src/shared_environment.py
@@ -3,6 +3,7 @@ import sys
 import glob
 import shutil
 import timeit
+from abc import ABCMeta, abstractmethod
 if os.name == 'posix' and sys.version_info[0] < 3:
     try:
         import subprocess32 as subprocess
@@ -13,8 +14,10 @@ else:
 import util
 from shared_diagnostic import Diagnostic
 
-class EnvironmentManager:
-    # analogue of TestSuite in xUnit
+class EnvironmentManager(object):
+    # analogue of TestSuite in xUnit - abstract base class
+    __metaclass__ = ABCMeta
+
     def __init__(self, config, verbose=0):
         if 'pod_list' in config['case_list'][0]:
             # run a set of PODs specific to this model
@@ -31,8 +34,26 @@ class EnvironmentManager:
             self.pods.append(pod)
 
     # -------------------------------------
+    # following are specific details that must be implemented in child class 
 
-    def setUp(self, verbose=0):
+    @abstractmethod
+    def create_environment(self):
+        pass 
+
+    @abstractmethod
+    def set_pod_env(self, pod):
+        pass 
+
+    @abstractmethod
+    def activate_env_command(self, pod):
+        pass 
+
+    @abstractmethod
+    def deactivate_env_command(self, pod):
+        pass 
+
+    @abstractmethod
+    def destroy_environment(self):
         pass 
 
     # -------------------------------------
@@ -46,38 +67,34 @@ class EnvironmentManager:
             if verbose > 0: print("--- MDTF.py Starting POD "+pod.name+"\n")
 
             pod.setUp()
+            # skip this pod if missing data
             if pod.missing_files != []:
-                print "WARNING: POD ",pod.name," Not executed because missing required input files:"
-                print pod.missing_files
                 continue
-            else:
-                if (verbose > 0): print "No known missing required input files"
 
             pod.logfile_obj = open(os.path.join(os.environ["WK_DIR"], pod.name+".log"), 'w')
 
-            command_str = pod.run_command()            
+            run_command = pod.run_command()          
             if config['envvars']['test_mode']:
-                command_str = 'echo "TEST MODE: would call {}"'.format(command_str)
-            
+                run_command = 'echo "TEST MODE: would call {}"'.format(run_command)
+            commands = [
+                self.activate_env_command(pod), pod.validate_command(), 
+                run_command, self.deactivate_env_command(pod)
+                ]
+            # '&&' so we abort if any command in the sequence fails.
+            commands = ' && '.join([s for s in commands if s])
+ 
+            print("Calling :  "+run_command) # This is where the POD is called #
+            print('Will run in env: '+pod.env)
             start_time = timeit.default_timer()
             try:
-                print("Calling :  "+command_str) # This is where the POD is called #
-                print('Will run in conda env: '+pod.conda_env)
-                # Details on this invocation: Need to run bash explicitly because 
-                # 'conda activate' sources env vars (can't do that in posix sh).
-                # tcsh would also work. Source conda_init.sh to set things that 
-                # aren't set b/c we aren't in an interactive shell. '&&' so we abort 
-                # and don't try to run the POD if 'conda activate' fails.
-                pod.process_obj = subprocess.Popen([
-                    'bash', '-c',
-                    'source '+os.environ['DIAG_HOME']+'/src/conda_init.sh' \
-                    + ' && conda activate ' + pod.conda_env \
-                    + ' && ' + pod.validate_command() \
-                    + ' && ' + command_str],
+                # Need to run bash explicitly because 'conda activate' sources 
+                # env vars (can't do that in posix sh). tcsh could also work.
+                pod.process_obj = subprocess.Popen(
+                    ['bash', '-c', commands],
                     env=os.environ, stdout=pod.logfile_obj, stderr=subprocess.STDOUT)
             except OSError as e:
                 print('ERROR :',e.errno,e.strerror)
-                print(" occured with call: " +command_str)
+                print(" occured with call: " +run_command)
 
         # if this were python3 we'd have asyncio, instead wait for each process
         # to terminate and close all log files
@@ -95,3 +112,50 @@ class EnvironmentManager:
         # call diag's tearDown to clean up
         for pod in self.pods:
             pod.tearDown()
+        self.destroy_environment()
+
+
+class UnmanagedEnvironment(EnvironmentManager):
+    # Do not attempt to switch execution environments for each POD.
+    def create_environment(self):
+        pass 
+    
+    def destroy_environment(self):
+        pass 
+
+    def set_pod_env(self, pod):
+        pass
+
+    def activate_env_command(self, pod):
+        return ''
+
+    def deactivate_env_command(self, pod):
+        return '' 
+
+
+class CondaEnvironmentManager(EnvironmentManager):
+    # Use Anaconda to switch execution environments.
+    def create_environment(self):
+        pass 
+
+    def destroy_environment(self):
+        pass 
+
+    def set_pod_env(self, pod):
+        keys = [s.lower() for s in pod.programs]
+        if ('r' in keys) or ('rscript' in keys):
+            pod.env = '_MDTF-diagnostics-R'
+        elif 'ncl' in keys:
+            pod.env = '_MDTF-diagnostics-NCL'
+        else:
+            pod.env = '_MDTF-diagnostics'
+
+    def activate_env_command(self, pod):
+        # Source conda_init.sh to set things that aren't set b/c we aren't 
+        # in an interactive shell. 
+        return 'source {}/src/conda_init.sh && conda activate {}'.format(
+            os.environ['DIAG_HOME'], pod.env
+            )
+
+    def deactivate_env_command(self, pod):
+        return '' 

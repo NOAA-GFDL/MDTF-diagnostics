496e2d43c9f04c3de105dc9733fa00ff5d9585fa
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-10-10 10:16:42 -0400

    Fix indentation in EOF_500hPa driver

--- diagnostics/EOF_500hPa/EOF_500hPa.py
+++ diagnostics/EOF_500hPa/EOF_500hPa.py
@@ -12,46 +12,46 @@ import subprocess
 # generate_ncl_plots - call a nclPlotFile via subprocess call
 #============================================================
 def generate_ncl_plots(nclPlotFile):
-   """generate_plots_call - call a nclPlotFile via subprocess call
+    """generate_plots_call - call a nclPlotFile via subprocess call
    
-   Arguments:
-   nclPlotFile (string) - full path to ncl plotting file name
-   """
-   # check if the nclPlotFile exists - 
-   # don't exit if it does not exists just print a warning.
-   try:
-      pipe = subprocess.Popen(['ncl {0}'.format(nclPlotFile)], shell=True, stdout=subprocess.PIPE)
-      output = pipe.communicate()[0]
-      print('NCL routine {0} \n {1}'.format(nclPlotFile,output))            
-      while pipe.poll() is None:
-         time.sleep(0.5)
-   except OSError as e:
-      print('WARNING',e.errno,e.strerror)
+    Arguments:
+    nclPlotFile (string) - full path to ncl plotting file name
+    """
+    # check if the nclPlotFile exists - 
+    # don't exit if it does not exists just print a warning.
+    try:
+        pipe = subprocess.Popen(['ncl {0}'.format(nclPlotFile)], shell=True, stdout=subprocess.PIPE)
+        output = pipe.communicate()[0]
+        print('NCL routine {0} \n {1}'.format(nclPlotFile,output))            
+        while pipe.poll() is None:
+            time.sleep(0.5)
+    except OSError as e:
+        print('WARNING',e.errno,e.strerror)
 
-   return 0
+    return 0
 
 print "Entered "+__file__
 filename1 = os.environ["DATADIR"]+"/mon/"+os.environ["CASENAME"]+"."+os.environ["zg_var"]+".mon.nc"
 print "Looking for"+filename1
 if not os.path.isfile( filename1 ):
-   print ("ERROR missing file "+filename1)
+    print ("ERROR missing file "+filename1)
 
 if os.path.isfile( filename1): 
-       print("geopotential height files found") 
-      print("computing EOF of geopotential height anomalies of 500 hPa")
+    print("geopotential height files found") 
+    print("computing EOF of geopotential height anomalies of 500 hPa")
 
 #============================================================
 # Call NCL code here
 #============================================================
-      print("COMPUTING ANOMALIES")
-      generate_ncl_plots(os.environ["POD_HOME"]+"/compute_anomalies.ncl")
+    print("COMPUTING ANOMALIES")
+    generate_ncl_plots(os.environ["POD_HOME"]+"/compute_anomalies.ncl")
 
-      print(" N ATLANTIC EOF PLOT")
-      generate_ncl_plots(os.environ["POD_HOME"]+"/eof_natlantic.ncl")
+    print(" N ATLANTIC EOF PLOT")
+    generate_ncl_plots(os.environ["POD_HOME"]+"/eof_natlantic.ncl")
 
-      print(" N PACIFIC EOF PLOT")
-      generate_ncl_plots(os.environ["POD_HOME"]+"/eof_npacific.ncl")
+    print(" N PACIFIC EOF PLOT")
+    generate_ncl_plots(os.environ["POD_HOME"]+"/eof_npacific.ncl")
 
 
 else:
-      print("height and surface pressure files NOT found, skip EOF of geopotential height anomalies of 500 hPa")        
+    print("height and surface pressure files NOT found, skip EOF of geopotential height anomalies of 500 hPa")        

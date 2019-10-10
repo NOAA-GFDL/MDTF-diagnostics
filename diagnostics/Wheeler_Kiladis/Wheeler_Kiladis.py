cded436ce1fd3e37fba647eeafc5beba73a68fa8
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-10-10 00:41:08 -0400

    Pass $LEV to wkSpaceTime_driver.ncl

--- diagnostics/Wheeler_Kiladis/Wheeler_Kiladis.py
+++ diagnostics/Wheeler_Kiladis/Wheeler_Kiladis.py
@@ -52,6 +52,7 @@ os.chdir(os.environ["DATADIR"])
 if os.path.isfile(os.environ["DATADIR"]+"/day/"+os.environ["CASENAME"]+"."+os.environ["rlut_var"]+".day.nc"):
    os.environ["file_WK"] = os.environ["CASENAME"]+"."+os.environ["rlut_var"]+".day.nc"
    os.environ["MVAR"] = os.environ["rlut_var"]
+   os.environ["LEV"] = "sfc"
    print("file of "+os.environ["rlut_var"]+" for Wheeler-Kiladis plots found, computing wave spectra")
    generate_ncl_plots(os.environ["POD_HOME"]+"/wkSpaceTime_driver.ncl")
 else:  
@@ -62,6 +63,7 @@ else:
 if os.path.isfile(os.environ["DATADIR"]+"/day/"+os.environ["CASENAME"]+"."+os.environ["pr_var"]+".day.nc"):
    os.environ["file_WK"] = os.environ["CASENAME"]+"."+os.environ["pr_var"]+".day.nc"
    os.environ["MVAR"] = os.environ["pr_var"]
+   os.environ["LEV"] = "sfc"
    print("file of "+os.environ["pr_var"]+" for Wheeler-Kiladis plots found, computing wave spectra")
    generate_ncl_plots(os.environ["POD_HOME"]+"/wkSpaceTime_driver.ncl")
 else:  
@@ -72,6 +74,7 @@ else:
 if os.path.isfile(os.environ["DATADIR"]+"/day/"+os.environ["CASENAME"]+"."+os.environ["omega500_var"]+".day.nc"):
    os.environ["file_WK"] = os.environ["CASENAME"]+"."+os.environ["omega500_var"]+".day.nc"
    os.environ["MVAR"] = os.environ["omega500_var"]
+   os.environ["LEV"] = "500"
    print("file of "+os.environ["omega500_var"]+" for Wheeler-Kiladis plots found, computing wave spectra")
    generate_ncl_plots(os.environ["POD_HOME"]+"/wkSpaceTime_driver.ncl")
 else:  
@@ -82,6 +85,7 @@ else:
 if os.path.isfile(os.environ["DATADIR"]+"/day/"+os.environ["CASENAME"]+"."+os.environ["u200_var"]+".day.nc"):
    os.environ["file_WK"] = os.environ["CASENAME"]+"."+os.environ["u200_var"]+".day.nc"
    os.environ["MVAR"] = os.environ["u200_var"]
+   os.environ["LEV"] = "200"
    print("file of "+os.environ["u200_var"]+" for Wheeler-Kiladis plots found, computing wave spectra")
    generate_ncl_plots(os.environ["POD_HOME"]+"/wkSpaceTime_driver.ncl")
 else:  
@@ -92,37 +96,8 @@ else:
 if os.path.isfile(os.environ["DATADIR"]+"/day/"+os.environ["CASENAME"]+"."+os.environ["u850_var"]+".day.nc"):
    os.environ["file_WK"] = os.environ["CASENAME"]+"."+os.environ["u850_var"]+".day.nc"
    os.environ["MVAR"] = os.environ["u850_var"]
+   os.environ["LEV"] = "850"
    print("file of "+os.environ["u850_var"]+" for Wheeler-Kiladis plots found, computing wave spectra")
    generate_ncl_plots(os.environ["POD_HOME"]+"/wkSpaceTime_driver.ncl")
 else:  
    print("file of "+os.environ["u850_var"]+" for Wheeler-Kiladis plots NOT found, skip computing wave spectra")
-
-#============================================================
-# Rename PS files
-#============================================================
-files = os.listdir(os.environ["WK_DIR"]+"/model/PS")
-a = 0
-while a < len(files):
-   file0 = files[a]
-   file1 = commands.getoutput("echo "+file0+"|sed -e s/"+os.environ["rlut_var"]+"/rlut/g")   
-   file2 = commands.getoutput("echo "+file0+"|sed -e s/"+os.environ["pr_var"]+"/pr/g")  
-   file3 = commands.getoutput("echo "+file0+"|sed -e s/"+os.environ["omega500_var"]+"/omega500/g") 
-   file4 = commands.getoutput("echo "+file0+"|sed -e s/"+os.environ["u200_var"]+"/u200/g") 
-   file5 = commands.getoutput("echo "+file0+"|sed -e s/"+os.environ["u850_var"]+"/u850/g")
-
-   if file0 != file1:
-      os.system("mv -f "+os.environ["WK_DIR"]+"/model/PS/"+file0+" "+os.environ["WK_DIR"]+"/model/PS/"+file1)
-
-   if file0 != file2:
-      os.system("mv -f "+os.environ["WK_DIR"]+"/model/PS/"+file0+" "+os.environ["WK_DIR"]+"/model/PS/"+file2)
-
-   if file0 != file3:
-      os.system("mv -f "+os.environ["WK_DIR"]+"/model/PS/"+file0+" "+os.environ["WK_DIR"]+"/model/PS/"+file3)
-
-   if file0 != file4:
-      os.system("mv -f "+os.environ["WK_DIR"]+"/model/PS/"+file0+" "+os.environ["WK_DIR"]+"/model/PS/"+file4)
-
-   if file0 != file5:
-      os.system("mv -f "+os.environ["WK_DIR"]+"/model/PS/"+file0+" "+os.environ["WK_DIR"]+"/model/PS/"+file5)
- 
-   a = a+1

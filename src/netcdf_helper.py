fe717cbf95c90c0088036c5ec58ae9eff9881ea3
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-10-15 12:07:13 -0400

    Remove tabs & other debugging

--- src/netcdf_helper.py
+++ src/netcdf_helper.py
@@ -3,40 +3,48 @@ import datelabel
 from util import run_command, check_executable
 
 class NetcdfHelper(object):
-    err_string = """NetcdfHelper is a stub meant to define method signatures for 
-    netcdf manipulation wrapper functions. If these functions are being called
-    directly, something's gone wrong.
-    """
     def __init__(self):
-    	raise NotImplementedError(err_string)
+        raise NotImplementedError(
+            """NetcdfHelper is a stub defining method signatures for netcdf 
+            manipulation wrapper functions, and shouldn't be called directly."""
+            )
 
     @staticmethod
     def cat_chunks(chunk_list, out_file, working_dir=None):
-    	raise NotImplementedError(err_string)
+        raise NotImplementedError(
+            """NetcdfHelper is a stub defining method signatures for netcdf 
+            manipulation wrapper functions, and shouldn't be called directly."""
+            )
 
     @staticmethod
     def crop_time_axis(time_var_name, date_range, in_file, 
         out_file=None, working_dir=None):   
-    	raise NotImplementedError(err_string) 
+        raise NotImplementedError(
+            """NetcdfHelper is a stub defining method signatures for netcdf 
+            manipulation wrapper functions, and shouldn't be called directly."""
+            )
 
     @staticmethod
     def extract_3d_slice():
-    	raise NotImplementedError(err_string) 
+        raise NotImplementedError(
+            """NetcdfHelper is a stub defining method signatures for netcdf 
+            manipulation wrapper functions, and shouldn't be called directly."""
+            )
 
 
 class NcoNetcdfHelper(NetcdfHelper):
     # Just calls command-line utilities, doesn't use PyNCO bindings
-	def __init__(self):
-		# check nco exists
+    def __init__(self):
+        # check nco exists
         if not check_executable('ncks'):
             raise OSError('NCO utilities not found on $PATH.')
 
     @staticmethod
-	def cat_chunks(chunk_list, out_file, working_dir=None):
+    def cat_chunks(chunk_list, out_file, working_dir=None):
         if working_dir is None:
             working_dir = os.getcwd()
-		# not running in shell, so can't use glob expansion.
-        util.run_command(['ncrcat', '-O'] + chunk_list + [out_file], 
+        # not running in shell, so can't use glob expansion.
+        run_command(['ncrcat', '-O'] + chunk_list + [out_file], 
             cwd=working_dir)
 
     @staticmethod
@@ -50,7 +58,7 @@ class NcoNetcdfHelper(NetcdfHelper):
         if working_dir is None:
             working_dir = os.getcwd()
         ncks_time_format = '%Y-%m-%d %H:%M:%S'
-        util.run_command(
+        run_command(
             ['ncks', '-O', '-d', "{},'{}','{}'".format(
                 time_var_name, 
                 date_range.start.strftime(ncks_time_format),

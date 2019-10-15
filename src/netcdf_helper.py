716ceec03834efd00c1e3a268c979c017bf43e4c
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-10-15 11:32:29 -0400

    Changed my mind, make netcdf helper a mixin

--- src/netcdf_helper.py
+++ src/netcdf_helper.py
@@ -1,73 +1,27 @@
 import os
-from abc import ABCMeta, abstractmethod, abstractproperty
-from types import FunctionType
 import datelabel
 from util import run_command, check_executable
 
-class abstractstatic(staticmethod):
-    """Mark static methods as abstract, since we can't combine the decorators in
-    python < 3.3.
-
-    Source: https://stackoverflow.com/a/4474495
-    """
-    __slots__ = ()
-    def __init__(self, function):
-        super(abstractstatic, self).__init__(function)
-        function.__isabstractmethod__ = True
-    __isabstractmethod__ = True
-
-class ABCMetaTypeCheck(ABCMeta):
-    """Throw error if child classes try to implement abstract methods with
-    methods of the wrong type. Unfortunately there doesn't seem to be a way
-    to check if the child's method has the same signature as parent, which is
-    what we really want.
-
-    Source: https://stackoverflow.com/q/29455660
-    """
-    _typemap = {  # map abstract type to expected implementation type
-        abstractproperty: property,
-        abstractstatic: staticmethod,
-        # abstractmethods return function objects
-        FunctionType: FunctionType,
-    }
-    def __new__(mcls, name, bases, namespace):
-        cls = super(ABCMetaTypeCheck, mcls).__new__(mcls, name, bases, namespace)
-        wrong_type = set()
-        seen = set()
-        abstractmethods = cls.__abstractmethods__
-        for base in bases:
-            for name in getattr(base, "__abstractmethods__", set()):
-                if name in seen or name in abstractmethods:
-                    continue  # still abstract or later overridden
-                value = base.__dict__.get(name)  # bypass descriptors
-                if getattr(value, "__isabstractmethod__", False):
-                    seen.add(name)
-                    expected = mcls._typemap[type(value)]
-                    if not isinstance(namespace[name], expected):
-                        wrong_type.add(name)
-        if wrong_type:
-            cls.__abstractmethods__ = abstractmethods | frozenset(wrong_type)
-        return cls
-
 class NetcdfHelper(object):
-    __metaclass__ = ABCMetaTypeCheck
-
+    err_string = """NetcdfHelper is a stub meant to define method signatures for 
+    netcdf manipulation wrapper functions. If these functions are being called
+    directly, something's gone wrong.
+    """
     def __init__(self):
-    	pass
+    	raise NotImplementedError(err_string)
 
-    @abstractstatic
+    @staticmethod
     def cat_chunks(chunk_list, out_file, working_dir=None):
-        pass
+    	raise NotImplementedError(err_string)
 
-    @abstractstatic
+    @staticmethod
     def crop_time_axis(time_var_name, date_range, in_file, 
-        out_file=None, working_dir=None):
-        pass 
+        out_file=None, working_dir=None):   
+    	raise NotImplementedError(err_string) 
 
-    @abstractstatic
+    @staticmethod
     def extract_3d_slice():
-        pass 
-
+    	raise NotImplementedError(err_string) 
 
 
 class NcoNetcdfHelper(NetcdfHelper):
@@ -76,7 +30,6 @@ class NcoNetcdfHelper(NetcdfHelper):
 		# check nco exists
         if not check_executable('ncks'):
             raise OSError('NCO utilities not found on $PATH.')
-		super(NcoNetcdfHelper, self).__init__()
 
     @staticmethod
 	def cat_chunks(chunk_list, out_file, working_dir=None):
@@ -105,24 +58,6 @@ class NcoNetcdfHelper(NetcdfHelper):
             ), in_file, out_file],
             cwd=working_dir)
 
-    @staticmethod
-    def extract_3d_slice():
-        raise NotImplementedError 
-
 
 class CdoNetcdfHelper(NetcdfHelper):
-	def __init__(self):
-        raise NotImplementedError
-
-    @staticmethod
-	def cat_chunks(chunk_list, out_file, working_dir=None):
-        raise NotImplementedError 
-
-    @staticmethod
-    def crop_time_axis(time_var_name, date_range, in_file, 
-        out_file=None, working_dir=None):
-        raise NotImplementedError 
-
-    @staticmethod
-    def extract_3d_slice():
-        raise NotImplementedError 
+    pass

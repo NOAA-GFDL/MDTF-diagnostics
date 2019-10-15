7aa370127d6d742b92b696211be6641cd0d8cb89
tsjackson-noaa <thomas.jackson@noaa.gov>
2019-10-15 12:11:54 -0400

    Rename methods for clarity

--- src/netcdf_helper.py
+++ src/netcdf_helper.py
@@ -10,14 +10,18 @@ class NetcdfHelper(object):
             )
 
     @staticmethod
-    def cat_chunks(chunk_list, out_file, working_dir=None):
+    def nc_check_environ():
+        pass
+
+    @staticmethod
+    def nc_cat_chunks(chunk_list, out_file, working_dir=None):
         raise NotImplementedError(
             """NetcdfHelper is a stub defining method signatures for netcdf 
             manipulation wrapper functions, and shouldn't be called directly."""
             )
 
     @staticmethod
-    def crop_time_axis(time_var_name, date_range, in_file, 
+    def nc_crop_time_axis(time_var_name, date_range, in_file, 
         out_file=None, working_dir=None):   
         raise NotImplementedError(
             """NetcdfHelper is a stub defining method signatures for netcdf 
@@ -25,7 +29,7 @@ class NetcdfHelper(object):
             )
 
     @staticmethod
-    def extract_3d_slice():
+    def nc_extract_level():
         raise NotImplementedError(
             """NetcdfHelper is a stub defining method signatures for netcdf 
             manipulation wrapper functions, and shouldn't be called directly."""
@@ -34,13 +38,14 @@ class NetcdfHelper(object):
 
 class NcoNetcdfHelper(NetcdfHelper):
     # Just calls command-line utilities, doesn't use PyNCO bindings
-    def __init__(self):
+    @staticmethod
+    def nc_check_environ():
         # check nco exists
         if not check_executable('ncks'):
             raise OSError('NCO utilities not found on $PATH.')
 
     @staticmethod
-    def cat_chunks(chunk_list, out_file, working_dir=None):
+    def nc_cat_chunks(chunk_list, out_file, working_dir=None):
         if working_dir is None:
             working_dir = os.getcwd()
         # not running in shell, so can't use glob expansion.
@@ -48,7 +53,7 @@ class NcoNetcdfHelper(NetcdfHelper):
             cwd=working_dir)
 
     @staticmethod
-    def crop_time_axis(time_var_name, date_range, in_file, 
+    def nc_crop_time_axis(time_var_name, date_range, in_file, 
         out_file=None, working_dir=None):
         if out_file is None:
             # NCO v4.0.3 seems to handle overwriting existing file in-place OK

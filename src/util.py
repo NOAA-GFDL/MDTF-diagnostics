e1fc4a895121a8a1e12ce0ebb5c678ca89139340
Thomas Jackson <tom.jackson314@gmail.com>
2020-05-17 22:31:51 -0400

    Move resolve_path() to util.py

--- src/util.py
+++ src/util.py
@@ -371,6 +371,46 @@ def find_files(root_dir, pattern):
     prefix_length = len(os.path.normpath(root_dir)) + 1 
     return [p[prefix_length:] for p in paths]
 
+def resolve_path(path, root_path="", env=None):
+    """Abbreviation to resolve relative paths.
+
+    Args:
+        path (:obj:`str`): path to resolve.
+        root_path (:obj:`str`, optional): root path to resolve `path` with. If
+            not given, resolves relative to `cwd`.
+
+    Returns: Absolute version of `path`, relative to `root_path` if given, 
+        otherwise relative to `os.getcwd`.
+    """
+    def _expandvars(path, env_dict):
+        """Expand quoted variables of the form $key and ${key} in path,
+        where key is a key in env_dict, similar to os.path.expandvars.
+
+        See https://stackoverflow.com/a/30777398; specialize to not skipping
+        escaped characters and not changing unrecognized variables.
+        """
+        return re.sub(
+            r'\$(\w+|\{([^}]*)\})', 
+            lambda m: env_dict.get(m.group(2) or m.group(1), m.group(0)), 
+            path
+        )
+
+    if path == '':
+        return path # default value set elsewhere
+    path = os.path.expanduser(path) # resolve '~' to home dir
+    path = os.path.expandvars(path) # expand $VAR or ${VAR} for shell envvars
+    if isinstance(env, dict):
+        path = _expandvars(path, env)
+    if '$' in path:
+        print("Warning: couldn't resolve all env vars in '{}'".format(path))
+        return path
+    if os.path.isabs(path):
+        return path
+    if root_path == "":
+        root_path = os.getcwd()
+    assert os.path.isabs(root_path)
+    return os.path.normpath(os.path.join(root_path, path))
+
 def check_executable(exec_name):
     """Tests if <exec_name> is found on the current $PATH.
 

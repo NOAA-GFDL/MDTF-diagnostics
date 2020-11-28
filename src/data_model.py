f01e66b206bb9b21687b8b5e3a92e2308c77a865
tsjackson-noaa <thomas.jackson@noaa.gov>
2020-11-28 16:20:43 -0500

    Add coordinate bounds to data model

--- src/data_model.py
+++ src/data_model.py
@@ -9,7 +9,7 @@ import typing
 from src import util, datelabel
 
 DMAxis = util.MDTFEnum(
-    'DMAxis', 'X Y Z T OTHER', module=__name__
+    'DMAxis', 'X Y Z T BOUNDS OTHER', module=__name__
 )
 DMAxis.spatiotemporal_names = ('X', 'Y', 'Z', 'T')
 DMAxis.__doc__ = """:py:class:`~enum.Enum` encoding the recognized axis types
@@ -25,10 +25,12 @@ class DMCoordinate(object):
     standard_name: str
     units: str
     axis: DMAxis = DMAxis.OTHER
+    bounds: DMCoordinateBounds = None
 
 @util.mdtf_dataclass
 class DMLongitudeCoordinate(object):
     name: str
+    bounds: DMCoordinateBounds = None
     
     standard_name = 'longitude'
     units = 'degrees_E'
@@ -37,6 +39,7 @@ class DMLongitudeCoordinate(object):
 @util.mdtf_dataclass
 class DMLatitudeCoordinate(object):
     name: str
+    bounds: DMCoordinateBounds = None
 
     standard_name = 'latitude'
     units = 'degrees_N'
@@ -51,6 +54,7 @@ class DMVerticalCoordinate(object):
     standard_name: str
     units: str = "1" # dimensionless vertical coords OK
     positive: str
+    bounds: DMCoordinateBounds = None
 
     axis = DMAxis.Z
 
@@ -75,6 +79,7 @@ class DMTimeCoordinate(object):
     calendar: str
     range: datelabel.AbstractDateRange = None
     frequency: datelabel.AbstractDateFrequency = None
+    bounds: DMCoordinateBounds = None
 
     standard_name = 'time'
     axis = DMAxis.T
@@ -87,6 +92,19 @@ class DMTimeCoordinate(object):
         data source.
         """
         return (self.range == datelabel.FXDateRange)
+
+@util.mdtf_dataclass
+class DMBoundsDimension(object):
+    """Placeholder object to represent the bounds dimension of a 
+    :class:`DMCoordinateBounds` object. Not a dimension coordinate, and strictly
+    speaking we should make another set of classes for dimensions.
+    """
+    name: str
+    
+    standard_name = 'bounds'
+    units = '1'
+    axis = DMAxis.BOUNDS
+    bounds = None
 
 class AbstractDMCoordinate(abc.ABC):
     """Defines interface (set of attributes) for :class:`DMCoordinate` objects.
@@ -111,6 +129,11 @@ class AbstractDMCoordinate(abc.ABC):
     def axis(self):
         pass
 
+    @property
+    @abc.abstractmethod
+    def bounds(self):
+        pass
+
 # Use the "register" method, instead of inheritance, to identify these classes
 # as implementations of AbstractDMCoordinate, because Python dataclass 
 # fields aren't recognized as implementing an abc.abstractmethod.
@@ -120,6 +143,7 @@ AbstractDMCoordinate.register(DMLatitudeCoordinate)
 AbstractDMCoordinate.register(DMVerticalCoordinate)
 AbstractDMCoordinate.register(DMParametricVerticalCoordinate)
 AbstractDMCoordinate.register(DMTimeCoordinate)
+AbstractDMCoordinate.register(DMBoundsDimension)
 
 @util.mdtf_dataclass
 class DMScalarCoordinateMixin(object):
@@ -128,6 +152,15 @@ class DMScalarCoordinateMixin(object):
     <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.8/cf-conventions.html#scalar-coordinate-variables>`__.
     """
     value: typing.Union[int, float] = None
+
+    @classmethod
+    def from_coordinate(cls, coord, value):
+        if not issubclass(cls, coord.__class__):
+            raise ValueError((f"Tried to create instance of {cls} from "
+                f"different DMCoordinate type {coord}."))
+        kwargs = dataclasses.asdict(coord)
+        kwargs['value'] = value
+        return cls(**kwargs)
 
 @util.mdtf_dataclass
 class DMScalarCoordinate(DMCoordinate, DMScalarCoordinateMixin):
@@ -218,6 +251,40 @@ class DMAuxiliaryCoordinate(DMDimensions):
     name: str
     standard_name: str
     units: str
+
+@util.mdtf_dataclass
+class DMCoordinateBounds(DMAuxiliaryCoordinate):
+    """Class describing bounds on a dimension coordinate.
+    """
+    def __post_init__(self):
+        super(DMCoordinateBounds, self).__post_init__()
+        # validate dimensions
+        if self.scalar_coords:
+            raise ValueError(("Attempted to create DMCoordinateBounds "
+                f"{self.name} with scalar coordinates: {self.scalar_coords}."))
+        if len(self.dims) != 2 or \
+            DMAxis.BOUNDS not in {c.axis for c in self.dims}:
+            raise ValueError(("Attempted to create DMCoordinateBounds "
+                f"{self.name} with improper dimensions: {self.dims}."))
+
+    @property
+    def coord(self):
+        for c in self.dims:
+            if c.axis != DMAxis.BOUNDS:
+                return c
+        raise ValueError
+
+    @classmethod
+    def from_coordinate(cls, coord, bounds_dim):
+        kwargs = {attr: getattr(coord, attr) for attr \
+            in ('name', 'standard_name', 'units')}
+        if not isinstance(bounds_dim, DMBoundsDimension):
+            bounds_dim = DMBoundsDimension(name=bounds_dim)
+        kwargs['dims'] = (coord, bounds_dim)
+        kwargs['scalar_coords'] = set([])
+        coord_bounds = cls(**kwargs)
+        coord.bounds = coord_bounds
+        return coord_bounds
 
 @util.mdtf_dataclass
 class DMVariable(DMDimensions):

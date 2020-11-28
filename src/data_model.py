b110ee8a9149005911438a53f4642bd87834d267
tsjackson-noaa <thomas.jackson@noaa.gov>
2020-11-28 14:46:31 -0500

    Tweak defs of dimension coordinates in data model

--- src/data_model.py
+++ src/data_model.py
@@ -4,66 +4,73 @@ independent of any model, experiment, or hosting protocol.
 import abc
 import collections
 import dataclasses
+import itertools
 import typing
-from src import util, util_mdtf, datelabel
+from src import util, datelabel
 
 DMAxis = util.MDTFEnum(
     'DMAxis', 'X Y Z T OTHER', module=__name__
 )
+DMAxis.spatiotemporal_names = ('X', 'Y', 'Z', 'T')
+DMAxis.__doc__ = """:py:class:`~enum.Enum` encoding the recognized axis types
+(dimension coordinates with a distinguished role.)
+"""
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMCoordinate(object):
     """Class to describe a single coordinate variable (in the sense used by the
     `CF conventions <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.8/cf-conventions.html#terminology>`__).
     """
-    dim: str
+    name: str
     standard_name: str
     units: str
     axis: DMAxis = DMAxis.OTHER
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMLongitudeCoordinate(object):
-    dim: str
+    name: str
+    
     standard_name = 'longitude'
     units = 'degrees_E'
     axis = DMAxis.X
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMLatitudeCoordinate(object):
-    dim: str
+    name: str
+
     standard_name = 'latitude'
     units = 'degrees_N'
     axis = DMAxis.Y
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMVerticalCoordinate(object):
     """Class to describe a non-parametric vertical coordinate (height or depth),
     following the `CF conventions <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.8/cf-conventions.html#vertical-coordinate>`__.
     """
-    dim: str
+    name: str
     standard_name: str
     units: str = "1" # dimensionless vertical coords OK
     positive: str
 
     axis = DMAxis.Z
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMParametricVerticalCoordinate(DMVerticalCoordinate):
     """Class to describe `parametric vertical coordinates
     <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.8/cf-conventions.html#parametric-vertical-coordinate>`__.
     Note that the variable names appearing in ``formula_terms`` aren't parsed 
     here, in order to keep the class hashable. 
     """
-    dim: str
+    name: str
     computed_standard_name: str = ""
     long_name: str = ""
     formula_terms: str = dataclasses.field(default=None, compare=False)
     # Don't include formula_terms in testing for equality, since this could 
     # reference different names for the aux coord variables.
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMTimeCoordinate(object):
-    dim: str
+    name: str
     units: str
     calendar: str
     range: datelabel.AbstractDateRange = None
@@ -86,7 +93,7 @@ class AbstractDMCoordinate(abc.ABC):
     """
     @property
     @abc.abstractmethod
-    def dim(self):
+    def name(self):
         pass
 
     @property
@@ -114,6 +121,7 @@ AbstractDMCoordinate.register(DMVerticalCoordinate)
 AbstractDMCoordinate.register(DMParametricVerticalCoordinate)
 AbstractDMCoordinate.register(DMTimeCoordinate)
 
+@util.mdtf_dataclass
 class DMScalarCoordinateMixin(object):
     """Agument definitions of coordinates with a specific value, as in the
     CF conventions treatment of `scalar coordinates 
@@ -121,11 +129,11 @@ class DMScalarCoordinateMixin(object):
     """
     value: typing.Union[int, float] = None
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMScalarCoordinate(DMCoordinate, DMScalarCoordinateMixin):
     pass
 
-@util.mdtf_dataclass(frozen=True)
+@util.mdtf_dataclass
 class DMScalarVerticalCoordinate(DMVerticalCoordinate, DMScalarCoordinateMixin):
     pass
 
@@ -145,22 +153,51 @@ AbstractDMScalarCoordinate.register(DMScalarCoordinate)
 AbstractDMScalarCoordinate.register(DMScalarVerticalCoordinate)
 
 @util.mdtf_dataclass
-class DMCoordinateSet(object):
-    # TODO: stuff for map projections, non-lat-lon horiz bookkeeping
-    # also areacello/volcello, hyam/hybm
+class DMDimensions(object):
+    """Lookups for the dimensions, and associated dimension coordinates, 
+    associated with an array (eg a variable or auxiliary coordinate.)
+    """
     dims: tuple
-    scalar_coordinates: list
-    aux_coordinates: list
-    X: AbstractDMCoordinate = dataclasses.field(init=False, default=None)
-    Y: AbstractDMCoordinate = dataclasses.field(init=False, default=None)
-    Z: AbstractDMCoordinate = dataclasses.field(init=False, default=None)
-    T: AbstractDMCoordinate = dataclasses.field(init=False, default=None)
+    scalar_coords: set = dataclasses.field(default_factory=set)
+    axes: dict = dataclasses.field(init=False)
+    phys_axes: dict = dataclasses.field(init=False)
 
     def __post_init__(self):
-        for d in self.coords:
-            axis = getattr(d.axis, 'name', '')
-            if axis in ('X', 'Y', 'Z', 'T'):
-                setattr(self, axis, d) 
+        # validate that we don't have duplicate axes
+        temp_d = dict()
+        for c in itertools.chain(self.dims, self.scalar_coords):
+            axis = getattr(c.axis, 'name', '')
+            if axis != 'OTHER' and axis in temp_d:
+                raise ValueError((f"Duplicate definition of {axis} axis:"
+                        f"{str(c)}, {str(temp_d[axis])}"))
+            temp_d[axis] = c
+
+        self.axes = dict((x, None) for x in DMAxis.spatiotemporal_names)
+        for c in self.dims:
+            axis = getattr(c.axis, 'name', '')
+            if axis in DMAxis.spatiotemporal_names:
+                self.axes[axis] = c
+        self.phys_axes = self.axes.copy()
+        for c in self.scalar_coords:
+            axis = getattr(c.axis, 'name', '')
+            if axis in DMAxis.spatiotemporal_names:
+                self.phys_axes[axis] = c
+
+    @property
+    def X(self):
+        return self.axes['X']
+
+    @property
+    def Y(self):
+        return self.axes['Y']
+
+    @property
+    def Z(self):
+        return self.axes['Z']
+
+    @property
+    def T(self):
+        return self.axes['T']
 
     @property
     def is_static(self):
@@ -170,22 +207,22 @@ class DMCoordinateSet(object):
         assert not self.is_static
         new_T = dataclasses.replace(self.T, range=new_range)
         self.T = new_T
-        # TODO: still need to replace in dims
-
 
 @util.mdtf_dataclass
-class DMAuxiliaryCoordinate(DMCoordinateSet):
+class DMAuxiliaryCoordinate(DMDimensions):
     """Class to describe `auxiliary coordinate variables 
     <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.8/cf-conventions.html#terminology>`__,
     as defined in the CF conventions. An example would be lat or lon for data 
     presented in a tripolar grid projection.
     """
+    name: str
     standard_name: str
     units: str
 
 @util.mdtf_dataclass
-class DMVariable(DMCoordinateSet):
+class DMVariable(DMDimensions):
     """Class to describe general properties of data (dependent) variables.
     """
+    name: str
     standard_name: str
     units: str

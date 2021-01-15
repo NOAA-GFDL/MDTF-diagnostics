NOAA-GFDL/MDTF-diagnostics

commit 804f131514eca905c4aeb72970f8fbe0f0f9b343

Fix mutable fields in VarlistEntry
Put constructor for mutable fields (lists, etc) in VarlistEntry in
__post_init__ instead of default_factory. This fixes a bug where a
VarlistEntry created by calling dataclasses.replace() on a VE would
inherit its local_data, etc. fields, because the fields on the VEs point
to the object.

Dataclasses created by util.coerce_to_dataclass() aren't susceptible to
this bug, since that function calls the class __init__().

src/diagnostic.py
@@ -137,12 +137,10 @@ class VarlistEntry(data_model.DMVariable, _VarlistGlobalSettings):
     requirement: VarlistEntryRequirement = dc.field(
         default=VarlistEntryRequirement.REQUIRED, compare=False
     )
-    alternates: list = dc.field(default_factory=list, compare=False)
-    translation: typing.Any = dc.field(default=None, compare=False)
-    remote_data: util.WormDict = dc.field(
-        default_factory=util.WormDict, compare=False
-    )
-    local_data: list = dc.field(default_factory=list, compare=False)
+    alternates: list = dc.field(init=False, compare=False)
+    translation: typing.Any = dc.field(init=False, compare=False)
+    remote_data: util.WormDict = dc.field(init=False, compare=False)
+    local_data: list = dc.field(init=False, compare=False)
     status: VarlistEntryStatus = dc.field(
         default=VarlistEntryStatus.NOTSET, compare=False
     )
@@ -153,6 +151,14 @@ class VarlistEntry(data_model.DMVariable, _VarlistGlobalSettings):
         super(VarlistEntry, self).__post_init__(coords)
         self.active = (self.requirement == VarlistEntryRequirement.REQUIRED)
         self.exception = None
+        # instantiate mutable fields here so that if we copy VE (eg with .replace)
+        # the fields on the copy won't point to the same object as the fields on
+        # the original
+        self._id = util.NOTSET
+        self.alternates = []
+        self.translation = None
+        self.remote_data: util.WormDict()
+        self.local_data = []
 
         # env_vars
         if not self.env_var:
@@ -287,7 +293,7 @@ class VarlistEntry(data_model.DMVariable, _VarlistGlobalSettings):
 
     @property
     def full_name(self):
-        return '<' + self.pod_name + ':' + self.name + '>'
+        return f"<{self.pod_name}:{self.name} ({self._id})>"
 
     def iter_alternate_entries(self):
         """Iterator over all VarlistEntries referenced as parts of "sets" of 

commit c38c3adbcaeb07e8cb97b50f929370b6294ef92f

Fix initialization of VarlistEntry fields.
Fixes a bug introduced in 804f131514eca905c4aeb72970f8fbe0f0f9b343.
"alternates" field (at least) must not have init=False, since those
values need to be initialized from the POD's varlist.

Eliminate all init=False fields (since the dataclasses.py docs imply
this functionality is flaky) and instead re-initialize appropriate
fields in __post_init__, to avoid re-introducing the bug that
804f131514eca905c4aeb72970f8fbe0f0f9b343 fixed.

src/diagnostic.py
@@ -128,7 +128,7 @@ class VarlistEntry(data_model.DMVariable, _VarlistGlobalSettings):
     product, ie if the same output file from the preprocessor can be symlinked 
     to two different locations.
     """
-    _id: int = dc.field(init=False) # assigned by DataSource (avoids unsafe_hash)
+    _id: int = util.NOTSET # assigned by DataSource (avoids unsafe_hash)
     pod_name: str = ""
     use_exact_name: bool = False
     dest_path: str = ""
@@ -137,28 +137,26 @@ class VarlistEntry(data_model.DMVariable, _VarlistGlobalSettings):
     requirement: VarlistEntryRequirement = dc.field(
         default=VarlistEntryRequirement.REQUIRED, compare=False
     )
-    alternates: list = dc.field(init=False, compare=False)
-    translation: typing.Any = dc.field(init=False, compare=False)
-    remote_data: util.WormDict = dc.field(init=False, compare=False)
-    local_data: list = dc.field(init=False, compare=False)
+    alternates: list = dc.field(default_factory=list, compare=False)
+    translation: typing.Any = dc.field(default=None, compare=False)
+    remote_data: util.WormDict = dc.field(default_factory=util.WormDict, compare=False)
+    local_data: list = dc.field(default_factory=list, compare=False)
     status: VarlistEntryStatus = dc.field(
         default=VarlistEntryStatus.NOTSET, compare=False
     )
-    active: bool = dc.field(init=False, compare=False)
-    exception: Exception = dc.field(init=False, compare=False)
+    active: bool = dc.field(default=util.NOTSET, compare=False)
+    exception: Exception = dc.field(default=None, compare=False)
 
     def __post_init__(self, coords=None):
         super(VarlistEntry, self).__post_init__(coords)
-        self.active = (self.requirement == VarlistEntryRequirement.REQUIRED)
-        self.exception = None
-        # instantiate mutable fields here so that if we copy VE (eg with .replace)
+        # (re)initialize mutable fields here so that if we copy VE (eg with .replace)
         # the fields on the copy won't point to the same object as the fields on
-        # the original
-        self._id = util.NOTSET
-        self.alternates = []
+        # the original.
         self.translation = None
         self.remote_data: util.WormDict()
         self.local_data = []
+        if self.active == util.NOTSET:
+            self.active = (self.requirement == VarlistEntryRequirement.REQUIRED)
 
         # env_vars
         if not self.env_var:
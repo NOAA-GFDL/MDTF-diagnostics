4da4b1e6f577acad78ca1d31b5e3e8f6391aeed1
tsjackson-noaa <thomas.jackson@noaa.gov>
2021-02-04 03:17:58 -0500

    Remove python2, NCO dependencies of base conda envs
    PODS which run in NCL_base and R_base only use python for the "wrapper"
    used in gen 1 PODs. Convert these wrappers to python3 (the only POD
    needing changes was EOF_500hPa) so that all base conda envs, together,
    only need to install one version of python (3.7).
    
    Remove NCO dependency in NCL_base, as none of the NCO utilities are
    called by any of the gen 1 PODs. All functions that previously needed
    NCO are now performed by the preprocessor.

--- diagnostics/EOF_500hPa/EOF_500hPa.py
+++ diagnostics/EOF_500hPa/EOF_500hPa.py
@@ -31,21 +31,21 @@ def generate_ncl_plots(nclPlotFile):
 
     return 0
 
-print "Entered "+__file__
+print("Entered "+__file__)
 filename1 = os.environ["ZG_FILE"]
 filename2a = os.environ["ZG_HYBRID_SIGMA_FILE"]
 filename2b = os.environ["PS_FILE"]
-print "Looking for"+filename1
+print("Looking for "+filename1)
 if not os.path.isfile( filename1 ):
-    print ("ERROR missing file "+filename1)
+    print("ERROR missing file "+filename1)
     os.environ['USE_HYBRID_SIGMA'] = "1"
-print "Looking for"+filename2a
+print("Looking for "+filename2a)
 if not os.path.isfile( filename2a ):
-    print ("ERROR missing file "+filename2a)
+    print("ERROR missing file "+filename2a)
     os.environ['USE_HYBRID_SIGMA'] = "0"
-print "Looking for"+filename2b
+print("Looking for "+filename2b)
 if not os.path.isfile( filename2b ):
-    print ("ERROR missing file "+filename2b)
+    print("ERROR missing file "+filename2b)
 
 
 if os.path.isfile(filename1) \

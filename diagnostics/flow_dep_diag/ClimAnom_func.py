9a50ec69cbbfb379873416cc7d50ce6f7d513b28
Your Name <drewr@iri.columbia.edu>
2021-04-15 10:22:33 -0700

    editing environment variables

--- diagnostics/flow_dep_diag/ClimAnom_func.py
+++ diagnostics/flow_dep_diag/ClimAnom_func.py
@@ -7,6 +7,7 @@ import dask
 import cftime
 np.seterr(divide='ignore', invalid='ignore')
 
+time_var = os.environ["time_coord"]
 #function to calculate climatology anomalies for eah variable
 def climAnom(var_path, var_name):
 
@@ -14,14 +15,14 @@ def climAnom(var_path, var_name):
     va = var_name
 
     ds = xr.open_dataset(ddir, decode_times = True)
-    ds['time'] = ds.indexes['time'].to_datetimeindex()
+    ds[time_var] = ds.indexes[time_var].to_datetimeindex()
     # Drop 1 dimensional coordinates
     ds = ds.squeeze()
     da = ds[va]
     da_ensmean = da.copy()
     #would need to make envt var or something so you could use either 'time' or 'S' as coordinate name without an issue
-    da_day_clim = da_ensmean.groupby('time.dayofyear').mean('time')
-    da_day_anom = da.groupby('time.dayofyear') - da_day_clim
+    da_day_clim = da_ensmean.groupby('time.dayofyear').mean(time_var)
+    da_day_anom = da.groupby('{time_coord}.dayofyear').format(**os.environ) - da_day_clim
     da_day_anom = da_day_anom.drop('dayofyear')
 
     return da_day_anom

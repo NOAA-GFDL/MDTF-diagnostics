42fb69797bb26c97a9aa22fecc0a717b8c565e28
Drew Resnick <drewr@iri.columbia>
2021-04-27 10:21:38 -0700

    Saving climatology anomaly data as new files

--- diagnostics/flow_dep_diag/ClimAnom_func.py
+++ diagnostics/flow_dep_diag/ClimAnom_func.py
@@ -3,26 +3,22 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 from datetime import datetime
-#import dask
-#import cftime
 np.seterr(divide='ignore', invalid='ignore')
 
-time_var = os.environ["time_coord"]
+### Function to calculate climatology anomalies for each variable in the diagnostic.
+
+time_var = os.environ["time_coord"] #set environment variable for time equal to var for function
 #function to calculate climatology anomalies for eah variable
 def climAnom(var_path, var_name):
 
-    ddir = var_path
-    va = var_name
-
-    ds = xr.open_dataset(ddir, decode_times = True)
-    ds[time_var] = ds.indexes[time_var].to_datetimeindex()
+    ds = xr.open_dataset(var_path, decode_times = True)
+    ds[time_var] = ds.indexes[time_var].to_datetimeindex() #convert time to datetime so we can use groupby functionality
     # Drop 1 dimensional coordinates
     ds = ds.squeeze()
-    da = ds[va]
+    da = ds[var_name]
     da_ensmean = da.copy()
-    #would need to make envt var or something so you could use either 'time' or 'S' as coordinate name without an issue
     da_day_clim = da_ensmean.groupby('{time_coord}.dayofyear'.format(**os.environ)).mean(time_var) #.format(**os.environ)
     da_day_anom = da.groupby('{time_coord}.dayofyear'.format(**os.environ)) - da_day_clim
     da_day_anom = da_day_anom.drop('dayofyear')
-
+    da_day_anom.to_netcdf("{WK_DIR}/model/netCDF/".format(**os.environ) + var_name + "_climAnom.nc")
     return da_day_anom
--- diagnostics/flow_dep_diag/WeatherTypes.py
+++ diagnostics/flow_dep_diag/WeatherTypes.py
@@ -1,3 +1,4 @@
+#Python packages/ modules imported for the diagnostic
 import os
 import xarray as xr
 import numpy as np
@@ -8,10 +9,12 @@ import cartopy.crs as ccrs
 from cartopy import feature
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-# key PyWR functions and climAnom are imported here from function files
+# The following are function scripts which the diagnostic calls to run specific calculations
+# refer to PyWR.py and ClimAnom_func.py for details on imported functions
 from PyWR import *
 from ClimAnom_func import *
 
+#Setting variables equal to environment variables set by the diagnostic
 time_coord = os.environ["time_coord"]
 lat_coord = os.environ["lat_coord"]
 lon_coord = os.environ["lon_coord"]
@@ -21,13 +24,9 @@ tas_var = os.environ["T250_var"]
 
 def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t2m_path, t2m_var):
     reanalysis = climAnom(var_path=reanalysis_path, var_name=reanalysis_var).stack(T=[time_coord], grid=[lat_coord, lon_coord])
-#reanalysis = climAnom(var_path="/Users/drewr/mdtf/inputdata/model/QBOi.EXP1.AMIP.001/day/QBOi.EXP1.AMIP.001.Z250.day.nc", var_name='Z250').stack(T=['time'], grid=['lat', 'lon'])
     rainfall = climAnom(var_path=rainfall_path, var_name=rainfall_var).stack(T=[time_coord], grid=[lat_coord, lon_coord])
-#rainfall = climAnom(var_path="/Users/drewr/mdtf/inputdata/model/QBOi.EXP1.AMIP.001/day/QBOi.EXP1.AMIP.001.PRECT.day.nc", var_name='PRECT').stack(T=['time'], grid=['lat', 'lon'])
     t2m = climAnom(var_path=t2m_path, var_name=t2m_var).stack(T=[time_coord], grid=[lat_coord, lon_coord])
-#t2m = climAnom(var_path="/Users/drewr/mdtf/inputdata/model/QBOi.EXP1.AMIP.001/day/QBOi.EXP1.AMIP.001.T250.day.nc", var_name='T250').stack(T=['time'], grid=['lat', 'lon'])
     reanalysis = reanalysis.to_dataset()
-#reanalysis = reanalysis.assign_coords(P=(250))
     rainfall = rainfall.to_dataset()
     t2m = t2m.to_dataset()
     print("Climatology anomaly calculations completed.")
@@ -36,7 +35,6 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
     n_eof = get_number_eof(X=reanalysis[Z250_var].values, var_to_explain=0.9, plot=True)
     pca_model = PCA(n_components=n_eof).fit(reanalysis[Z250_var].values)
     reanalysis_pc = pca_model.transform(reanalysis[Z250_var].values)
-
 #perform clustering using manually specified number of clusters
     n_cluster = int(os.environ["NCLUSTER"])
     n_sim = int(os.environ["NSIM"])
@@ -45,7 +43,6 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
     print('The classifiability index is {}'.format(class_idx))
 #define KMeans object
     best_fit = KMeans(n_clusters=n_cluster, init=centroids[best_part, :, :], n_init=1, max_iter=1).fit(reanalysis_pc)
-
 #start reanalysis
     reanalysis_composite = reanalysis.copy()
     model_clust = best_fit.fit_predict(reanalysis_pc) # get centroids
@@ -57,7 +54,6 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
     reanalysis_composite['WT'] = weather_types
     reanalysis_composite = reanalysis_composite.groupby('WT').mean(dim='T').unstack('grid')[Z250_var]
     reanalysis_composite['M'] = 0
-
     print("Reanalysis completed.")
     wt_anomalies = [] # initialize empty list
     wt_anomalies.append(reanalysis_composite)
@@ -73,7 +69,6 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
     wt=weather_types.to_dataframe(name='WT')
     wt=wt+1
     wt_counts = wt.groupby('WT').size().div(wt['WT'].size)
-
     print("Beginning plotting..")
     #plotting
     xmin,xmax = reanalysis[lon_coord].min(), reanalysis[lon_coord].max()
@@ -86,14 +81,12 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
             nrows=3, ncols=len(wt_unique), subplot_kw={'projection': map_proj},
             figsize=figsize, sharex=True, sharey=True
         )
-
     for i,w in enumerate(wt_unique):
         def selector(ds):
             times = wt.loc[wt['WT'] == w].index.get_level_values(time_coord)
             ds = ds.sel(T = np.in1d(ds.unstack('T')[time_coord], times))
             ds = ds.mean(dim = 'T')
             return(ds)
-
         # Top row: geopotential height anomalies
         ax = axes[0, i]
         ax.set_title('WT {}: {:.1%} of days'.format(w, wt_counts.values[i]))
@@ -135,7 +128,6 @@ def weatherTypes(reanalysis_path, reanalysis_var, rainfall_path, rainfall_var, t
         ax.add_feature(feature.BORDERS)
         #ax.set_extent([-95, -70, -9, 5])
         ax.tick_params(colors='b')
-
     # # Add Colorbar
     plt.tight_layout()
     fig.subplots_adjust(right=0.94)

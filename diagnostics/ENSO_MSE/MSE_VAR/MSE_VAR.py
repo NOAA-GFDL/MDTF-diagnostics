aa0eaa3c17ca223767388b456711c177abe4aae1
jhafner2 <71046600+jhafner2@users.noreply.github.com>
2021-05-13 09:30:49 -1000

    Add files via upload

--- diagnostics/ENSO_MSE/MSE_VAR/MSE_VAR.py
+++ diagnostics/ENSO_MSE/MSE_VAR/MSE_VAR.py
@@ -1,9 +1,9 @@
 #
-#      The code at this level uses pre-calculated data from levels 1 and 2 
+#      The code at this level uses pre-calculated data from levels 1 and 2
 #     COMPOSITE and MSE.  It calculates the Moist Static Energy (MSE)
-#    variances and select variable co-variances.  Area averaged seasonal 
-#     MSE variances and co-variances are displayed in bar charts in 
-#     ~/wkdir/MDTF_$CASE  directories.   
+#    variances and select variable co-variances.  Area averaged seasonal
+#     MSE variances and co-variances are displayed in bar charts in
+#     ~/wkdir/MDTF_$CASE  directories.
 #
 #       Contact Information:
 #       PI :  Dr. H. Annamalai,
@@ -16,6 +16,19 @@
 #     This package is distributed under the LGPLv3 license (see LICENSE.txt)
 
 import numpy as np
+
+import sys
+
+import datetime
+
+import os
+shared_dir = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'shared'
+)
+sys.path.insert(0, shared_dir)
+
+
 from get_data_in import get_data_in
 from get_flux_in import get_flux_in
 from get_clima_in import get_clima_in
@@ -26,17 +39,6 @@ from moist_routine_variance import moisture_variance
 from get_parameters_in import get_parameters_in
 from get_anomaly import get_anomaly
 
-import sys
-import subprocess
-
-import datetime
- 
-import os
-shared_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    'shared'
-)
-sys.path.insert(0, shared_dir)
 from get_dimensions import get_dimensions
 from get_lon_lat_plevels_in import  get_lon_lat_plevels_in
 from generate_ncl_call import generate_ncl_call
@@ -44,7 +46,7 @@ from generate_ncl_call import generate_ncl_call
       This package is distributed under the LGPLv3 license (see LICENSE.txt)
 
       The top driver for the MSE component variances calculations
-       all need to be anomalies !!! 
+       all need to be anomalies !!!
       input data are as follows:
        2 dimensional atmospheric variables: all vertical integrals
    dimensions:  IMAX, JMAX
@@ -58,8 +60,8 @@ from generate_ncl_call import generate_ncl_call
                MADV :  moisture advection [W/m2]
                OMSE  : MSE vertical advection [W/m2]
    1 dimensional INPUT:
-         LON(IMAX) - longitude dimensions 
-         LAT(JMAX) - latitude dimensions 
+         LON(IMAX) - longitude dimensions
+         LAT(JMAX) - latitude dimensions
          PLEV(ZMAX) - vertical dimensions and  pressure levels [mb]
          REARTH  - radius of earth in  [m]
 
@@ -111,7 +113,7 @@ else:
     print ("   ")
     sys.exit()
 
-#################  set the default domain lat/lon boxes 
+#################  set the default domain lat/lon boxes
 ###   Central Pacific
 clon1 = 160.
 clon2 = 200.
@@ -119,7 +121,7 @@ clat1 = -10.
 clat2 = 5.
 ##  and  Eastern Pacific
 elon1 = 220.
-elon2 = 280. 
+elon2 = 280.
 elat1 = -5.
 elat2 = 5.
 
@@ -143,7 +145,7 @@ prefixout  =  mse_var_dir +"/netCDF/"
 prefixout1 =  mse_var_dir +"/netCDF/ELNINO/"
 prefixout2 =  mse_var_dir +"/netCDF/LANINA/"
 
-## 
+##
 
 rearth = 6378000.0
 dx = -9999.
@@ -151,21 +153,20 @@ dy = -9999.
 
 undef = float(1.1e+20)
 season = "NIL"
-model = "NIL"
+
 llon1 = undef
 llon2 = undef
 llat1 = undef
 llat2 = undef
 imindx1 = undef
 imindx2 = undef
-im1 = undef
-im2 = undef
+
 sigma = undef
 composite = 0
 composite24 = 0
 regression = 0
 correlation = 0
-## 
+##
 
 im1 = 12
 im2 = 14
@@ -174,7 +175,7 @@ iy1 = os.environ["FIRSTYR"]
 iy2 = os.environ["FIRSTYR"]
 model = os.environ["CASENAME"]
 
-###  reading  in selected  parameters  from parameter.txt file 
+###  reading  in selected  parameters  from parameter.txt file
 ##   read in parameters    and the actual array dimensions imax, jmax, zmax,
 ##    longitudes, latitudes,  plevels
 llon1, llon2, llat1, llat2, sigma, imindx1, imindx2,  composite, im1, im2, season,  composite24, regression, correlation,  undef =  get_parameters_in(llon1, llon2, llat1, llat2, sigma, imindx1, imindx2, composite, im1, im2, season, composite24, regression, correlation,  undef, prefix)
@@ -193,7 +194,7 @@ plevs  = np.zeros(zmax,dtype='float32')
 
 lon, lat, plevs = get_lon_lat_plevels_in( imax, jmax, zmax, lon, lat, plevs, prefix1)
 
-###  array declarations 
+###  array declarations
 # 2D variables
 ts   = np.zeros((imax,jmax),dtype='float32', order='F')
 pr   = np.zeros((imax,jmax),dtype='float32', order='F')
@@ -231,12 +232,12 @@ tadv_var = undef
 ##   reading in climatology of MSE components
 
 mse_clim, omse_clim, madv_clim, tadv_clim =  get_clima_in(imax, jmax, mse_clim, omse_clim, madv_clim,  tadv_clim, prefix2, undef)
-##   reading in  climatological  fluxes 
+##   reading in  climatological  fluxes
 pr_clim, ts_clim, lhf_clim, shf_clim, sw_clim, lw_clim = get_clima_flux_in(imax, jmax,  pr_clim, ts_clim, lhf_clim, shf_clim, sw_clim, lw_clim, prefix12, undef)
 
 ######################
 ###   El Nino/La Nina composites   for default domain NINO3.4 + general domain
-###      general domain set by slon1, slon2, slat1, slat2  enviromental variables 
+###      general domain set by slon1, slon2, slat1, slat2  enviromental variables
 ###      under  MSE_VAR  section in ~/ENSO_MSE/ENSO_MSE.py
 slon1 =  os.environ["slon1"]
 slon2 =  os.environ["slon2"]
@@ -270,14 +271,14 @@ if( composite == 1):
     tadv  = get_anomaly(imax, jmax, zmax,  tadv, tadv_clim, undef)
 
 ## variance and co-variance calculations for default domains Central and Eastern Pacific:
-###        Central Pacif: 
+###        Central Pacif:
     shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var  =  moisture_variance(imax, jmax, zmax, clon1, clon2, clat1, clat2, lon, lat, plevs, ts, pr, shf, lhf, sw, lw, mse, madv, omse, tadv, shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  undef)
 
-####    output written out 
+####    output written out
     nameout  = 'MSE_variance_C.out'
     write_out(imax, jmax, zmax,  shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  prefixout1,  nameout,  undef)
 
-####  repeat for the Eastern 
+####  repeat for the Eastern
     shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var  =  moisture_variance(imax, jmax, zmax, elon1, elon2, elat1, elat2, lon, lat, plevs, ts, pr, shf, lhf, sw, lw, mse, madv, omse, tadv, shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  undef)
 
 ####    output written out
@@ -289,18 +290,18 @@ if( composite == 1):
 ##  the same calculations for user selected  domain
     shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var  =  moisture_variance(imax, jmax, zmax, slon1, slon2, slat1, slat2, lon, lat, plevs, ts, pr, shf, lhf, sw, lw, mse, madv, omse, tadv, shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  undef)
 
-###  selected domain data output 
+###  selected domain data output
     write_out_general(imax, jmax, zmax,  shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  prefixout1, undef)
 
-########   La Nina case   similar as for El Nino case 
+########   La Nina case   similar as for El Nino case
 ######
-##          reading data in 
+##          reading data in
 #############################################
     mse, omse, madv,  tadv  =  get_data_in(imax, jmax,  mse, omse, madv, tadv, prefix222, undef)
 
     pr, ts, lhf, shf, sw, lw = get_flux_in(imax, jmax,  pr, ts, lhf, shf, sw, lw, prefix111, undef)
 
-###    anomaly calculations 
+###    anomaly calculations
     pr = get_anomaly(imax, jmax, zmax,  pr, pr_clim, undef)
     ts = get_anomaly(imax, jmax, zmax,  ts, ts_clim, undef)
     shf = get_anomaly(imax, jmax, zmax,  shf, shf_clim, undef)
@@ -332,7 +333,7 @@ if( composite == 1):
 
     write_out_general(imax, jmax, zmax,  shf_var, lhf_var, sw_var, lw_var, mse_var, madv_var, omse_var, tadv_var,  prefixout2, undef)
 
-###########  plot the default domain NINO3.4 bar plots  
+###########  plot the default domain NINO3.4 bar plots
     generate_ncl_call(os.environ["POD_HOME"]+ "/MSE_VAR/NCL/plot_bars_composite.ncl")
 
 ##      plotting for the  user selected domain :
@@ -352,8 +353,8 @@ if os.path.isfile( file ):
 os.system("cp "+os.environ["POD_HOME"]+"/MSE_VAR/MSE_VAR.html " + file)
 
 #============================================================
-# DRB: no longer necessary to add a link to top index.html 
-# because it is linked from the ENSO_MSE/index.html 
+# DRB: no longer necessary to add a link to top index.html
+# because it is linked from the ENSO_MSE/index.html
 #============================================================
 
 
--- diagnostics/ENSO_MSE/MSE_VAR/get_anomaly.py
+++ diagnostics/ENSO_MSE/MSE_VAR/get_anomaly.py
@@ -1,6 +1,3 @@
-import numpy as np
-import os.path
-
 def get_anomaly(imax, jmax, zmax,  varin, varin_clim, undef):
  
     varin = varin - varin_clim   

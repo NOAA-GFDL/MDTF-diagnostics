7d26e618f1b6847a8697a3fa502a0ee3d457d201
jhafner2 <71046600+jhafner2@users.noreply.github.com>
2021-05-13 14:49:31 -1000

    Add files via upload

--- diagnostics/ENSO_MSE/shared/get_season.py
+++ diagnostics/ENSO_MSE/shared/get_season.py
@@ -6,15 +6,15 @@ def get_season( season, prefix):
 
     if (os.path.exists(prefix+"../shared/parameters.txt")):
         file = open(prefix+'../shared/parameters.txt', 'r')
-        for i in range(1, 12):
+        for i in range(1, 13):
             line = file.readline()
             line = line.strip()
             column = line.split()
-
-        line = file.readline()
-        line = line.strip()
-        column = line.split()
-        season =  column[2]
+	     season =  column[2]
+#        line = file.readline()
+#        line = line.strip()
+#        column = line.split()
+#        season =  column[2]
 
         file.close()
 

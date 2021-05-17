28d05944123e62420529d0a77bb26ac20851c2cb
Drew Resnick <drewr@iri.columbia>
2021-05-17 15:24:24 -0400

    adding documentation to file used for preprocessing of data

---
+++ diagnostics/flow_dep_diag/cropping.py
@@ -0,0 +1,26 @@
+import xarray as xr
+
+file_path =
+output_path =
+
+#United States BB; replace with region of interest
+#lon in range -180,180
+nla=71.36
+sla=18.91
+wlo=-171.79
+elo=-66.96
+ds = xr.open_dataset(file_path, decode_times=False)
+
+#the diagnostic assumes your grid has a longitudinal range -180,180
+#Shift your grid from 0,360 using the following lines
+ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180 #shift the values
+ds = ds.sortby(ds.lon) #sort them
+
+#in the case your data is not cropped to a
+#specific region, this code can be used to select a subset of data within a lat/lon
+
+mask_lon = (ds.lon >= wlo) & (ds.lon <= elo)
+mask_lat = (ds.lat >= sla) & (ds.lat <= nla)
+cropped_ds = ds.where(mask_lon & mask_lat, drop=True)
+
+cropped_ds.to_netcdf(output_path)

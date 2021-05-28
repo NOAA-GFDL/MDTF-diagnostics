9cfc77bd1841a161dc7b3424c39614934717ebf3
climate_kid <61133063+jiacheng-atmos@users.noreply.github.com>
2021-05-28 12:52:04 -0500

    Update top_heaviness_ratio_calculation.py

--- diagnostics/top_heaviness_metric/top_heaviness_ratio_calculation.py
+++ diagnostics/top_heaviness_metric/top_heaviness_ratio_calculation.py
@@ -14,16 +14,105 @@ lat_coord = os.environ["lat_coord"]
 lon_coord = os.environ["lon_coord"]
 lev_coord = os.environ["lev_coord"]
 omega_var = os.environ["omega_var"] 
+WK_DIR    = os.environ["WK_DIR"] 
+OBS_DATA  = os.environ["OBS_DATA"] 
+CASENAME  = os.environ["CASENAME"] 
 
-
-def top_heaviness_ratio_calculation(reanalysis_path, reanalysis_var):
+# run model data
+def top_heaviness_ratio_calculation_model(reanalysis_path, reanalysis_var):
     # read omega and land-sea fraction data
     ds= xr.open_dataset(reanalysis_path)
-    lev_obs=ds[lev_coord].values
-    lat_obs=ds[lat_coord].values
-    lon_obs=ds[lon_coord].values
+    lev_model=ds[lev_coord].values
+    lat_model=ds[lat_coord].values
+    lon_model=ds[lon_coord].values
+    isort=np.argsort(lev_model)[::-1] # descending
+    mid_omega_model=ds[reanalysis_var].values # mon x lev x lat x lon; for the sample data (July over 2000-2019) 
+    mid_omega_model=mid_omega_model[:,isort]
+    #======================deriving O1 and O2=======================
+    # construct Q1_model as half a sine wave and Q2_model as a full sine wave
+    # two base functions; Q1: idealized deep convection profile; Q2: Deep stratiform profile (Back et al. 2017)
+    Q1_model=np.zeros(len(lev_model))
+    Q2_model=np.zeros(len(lev_model))
+    dp=lev_model[-1]-lev_model[0]
+    for i in range(len(lev_model)):
+        Q1_model[i]=-np.sin(np.pi*(lev_model[i]-lev_model[0])/dp)
+        Q2_model[i]=np.sin(2*np.pi*(lev_model[i]-lev_model[0])/dp)
+    #Normalize 
+    factor=integrate.trapz(Q1_model*Q1_model,lev_model)/dp
+    Q1_model=Q1_model/np.sqrt(factor)
+    factor=integrate.trapz(Q2_model*Q2_model,lev_model)/dp
+    Q2_model=Q2_model/np.sqrt(factor)
+    # deriving O1 and O2; O1 and O2 are coefs of Q1 and Q2
+    mid_omega_model_ltm=np.nanmean(mid_omega_model,axis=0)
+    O1_model=integrate.trapz(mid_omega_model_ltm*Q1_model[:,None,None],lev_model,axis=0)/dp
+    O2_model=integrate.trapz(mid_omega_model_ltm*Q2_model[:,None,None],lev_model,axis=0)/dp
+    #====================== set up figures =======================
+    #====================== O1 =======================
+    fig, axes = plt.subplots(figsize=(8,4))
+    ilat=np.argsort(lat_model)
+    ilon=np.argsort(lon_model)
+    x,y = np.meshgrid(lon_model,lat_model) 
+    m = Basemap(projection="cyl",llcrnrlat=lat_model[ilat][0],urcrnrlat=lat_model[ilat][-1],\
+            llcrnrlon=lon_model[ilon][0],urcrnrlon=lon_model[ilon][-1],ax=axes,resolution='c')
+    m.drawcoastlines(linewidth=1, color="k")
+    m.drawparallels(np.arange(lat_model[ilat][0],lat_model[ilat][-1],30),labels=[1,0,0,0],linewidth=0.,fontsize=16)
+    m.drawmeridians(np.arange(lon_model[ilon][0],lon_model[ilon][-1],60),labels=[0,0,0,1],linewidth=0.,fontsize=16)
+    X,Y = m(x,y)
+    clevs=np.arange(-0.06,0.07,0.01)
+    im0 = m.contourf(X,Y,O1_model,clevs,cmap = plt.get_cmap('RdBu_r'),extend='both')
+    cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
+    axes.set_title('O1 [Pa/s]',loc='center',fontsize=16)
+    fig.tight_layout() 
+    fig = fig.savefig(WK_DIR+"/model/"+CASENAME+"_O1.png", format='png',bbox_inches='tight')
+    #====================== O2 =======================
+    fig, axes = plt.subplots(figsize=(8,4))
+    x,y = np.meshgrid(lon_model,lat_model) 
+    m = Basemap(projection="cyl",llcrnrlat=lat_model[ilat][0],urcrnrlat=lat_model[ilat][-1],\
+            llcrnrlon=lon_model[ilon][0],urcrnrlon=lon_model[ilon][-1],ax=axes,resolution='c')
+    m.drawcoastlines(linewidth=1, color="k")
+    m.drawparallels(np.arange(lat_model[ilat][0],lat_model[ilat][-1],30),labels=[1,0,0,0],linewidth=0.,fontsize=16)
+    m.drawmeridians(np.arange(lon_model[ilon][0],lon_model[ilon][-1],60),labels=[0,0,0,1],linewidth=0.,fontsize=16)
+    X,Y = m(x,y)
+    clevs=np.arange(-0.06,0.07,0.01)
+    im0 = m.contourf(X,Y,O2_model,clevs,cmap = plt.get_cmap('RdBu_r'),extend='both') 
+    cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
+    axes.set_title('O2 [Pa/s]',loc='center',fontsize=16)
+    fig.tight_layout()
+    fig = fig.savefig(WK_DIR+"/model/"+CASENAME+"_O2.png", format='png',bbox_inches='tight')    
+    #====================== O2/O1 top-heaviness ratio =======================
+    fig, axes = plt.subplots(figsize=(8,4))
+    mmid1=O2_model/O1_model
+    midi=O1_model<0.01   # We only investigate areas with O1 larger than zero
+    mmid1[midi]=np.nan
+    x,y = np.meshgrid(lon_model,lat_model)
+    m = Basemap(projection="cyl",llcrnrlat=lat_model[ilat][0],urcrnrlat=lat_model[ilat][-1],\
+            llcrnrlon=lon_model[ilon][0],urcrnrlon=lon_model[ilon][-1],ax=axes,resolution='c')
+    m.drawcoastlines(linewidth=1, color="k")
+    m.drawparallels(np.arange(lat_model[ilat][0],lat_model[ilat][-1],30),labels=[1,0,0,0],linewidth=0.,fontsize=16)
+    m.drawmeridians(np.arange(lon_model[ilon][0],lon_model[ilon][-1],60),labels=[0,0,0,1],linewidth=0.,fontsize=16)
+    X,Y = m(x,y)
+    clevs=np.arange(-0.6,0.7,0.1)
+    im0 = m.contourf(X,Y,mmid1,clevs,cmap = plt.get_cmap('RdBu_r'),extend='both') 
+    cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
+    axes.set_title('Top-heaviness Ratio (O2/O1)',loc='center',fontsize=18)
+    fig = fig.savefig(WK_DIR+"/model/"+CASENAME+"_Top_Heaviness_Ratio.png", format='png',bbox_inches='tight') 
+    print("Plotting Completed")
+        
+
+top_heaviness_ratio_calculation_model(os.environ["OMEGA_FILE"],os.environ["omega_var"])
+
+
+# run obs data (ERA5 2000-2019 July only)
+OBS_DATA = os.environ["OBS_DATA"]
+
+def top_heaviness_ratio_calculation_obs(obs_data_full_dir):
+    # read omega and land-sea fraction data
+    ds= xr.open_dataset(obs_data_full_dir)
+    lev_obs=ds['lev'].values
+    lat_obs=ds['lat'].values
+    lon_obs=ds['lon'].values
     isort=np.argsort(lev_obs)[::-1] # descending
-    mid_omega_obs=ds[reanalysis_var].values # mon x lev x lat x lon; for the sample data (July over 2000-2019) 
+    mid_omega_obs=ds['omega'].values # mon x lev x lat x lon; for the sample data (July over 2000-2019) 
     mid_omega_obs=mid_omega_obs[:,isort]
     #======================deriving O1 and O2=======================
     # construct Q1_obs as half a sine wave and Q2_obs as a full sine wave
@@ -60,7 +149,7 @@ def top_heaviness_ratio_calculation(reanalysis_path, reanalysis_var):
     cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
     axes.set_title('O1 [Pa/s]',loc='center',fontsize=16)
     fig.tight_layout() 
-    fig = fig.savefig("{WK_DIR}/model/Long_term_mean_of_O1.png", format='png',bbox_inches='tight')
+    fig = fig.savefig(WK_DIR+"/obs/ERA5_O1_2000_2019_July.png", format='png',bbox_inches='tight')
     #====================== O2 =======================
     fig, axes = plt.subplots(figsize=(8,4))
     x,y = np.meshgrid(lon_obs,lat_obs) 
@@ -75,7 +164,7 @@ def top_heaviness_ratio_calculation(reanalysis_path, reanalysis_var):
     cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
     axes.set_title('O2 [Pa/s]',loc='center',fontsize=16)
     fig.tight_layout()
-    fig = fig.savefig("{WK_DIR}/model/Long_term_mean_of_O2.png", format='png',bbox_inches='tight')    
+    fig = fig.savefig(WK_DIR+"/obs/ERA5_O2_2000_2019_July.png", format='png',bbox_inches='tight')    
     #====================== O2/O1 top-heaviness ratio =======================
     fig, axes = plt.subplots(figsize=(8,4))
     mmid1=O2_obs/O1_obs
@@ -92,9 +181,7 @@ def top_heaviness_ratio_calculation(reanalysis_path, reanalysis_var):
     im0 = m.contourf(X,Y,mmid1,clevs,cmap = plt.get_cmap('RdBu_r'),extend='both') 
     cbar = fig.colorbar(im0, ax=axes, orientation="horizontal", pad=0.15,shrink=.9,aspect=45)
     axes.set_title('Top-heaviness Ratio (O2/O1)',loc='center',fontsize=18)
-    fig = fig.savefig("{WK_DIR}/model/Top_Heaviness_Ratio.png", format='png',bbox_inches='tight') 
-    
+    fig = fig.savefig(WK_DIR+"/obs/ERA5_Top_Heaviness_Ratio_2000_2019_July.png", format='png',bbox_inches='tight') 
     print("Plotting Completed")
-        
 
-top_heaviness_ratio_calculation(os.environ["OMEGA_FILE"],os.environ["omega_var"])
+top_heaviness_ratio_calculation_obs(OBS_DATA+'/ERA5_omega_mon_2000_2019_July.nc')

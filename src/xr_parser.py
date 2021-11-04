824191f2f10800ff78dc10c87105b75b7ebb1b07
Jess <20195932+wrongkindofdoctor@users.noreply.github.com>
2021-11-04 12:45:27 -0400

    Change cf_time calendar logic

--- src/xr_parser.py
+++ src/xr_parser.py
@@ -1117,10 +1117,12 @@ class DefaultDatasetParser():
 
         # normal case: T axis has been parsed into cftime Datetime objects, and
         # the following works successfully.
-        cftime_cal = getattr(t_coord.values[0], 'calendar', None)
+        cftime_cal = None
+        if 'calendar' in t_coord.values:
+            cftime_cal = getattr(t_coord.values, 'calendar', None)
         # look in other places if that failed:
         if not cftime_cal:
-            self.log.warning("cftime calendar info parse failed on '%s'.",
+            self.log.warning("cftime calendar info not available in t_coord.values in '%s'. Checking other t_coord elements.",
                 t_coord.name)
             cftime_cal = _get_calendar(t_coord.encoding)
         if not cftime_cal:

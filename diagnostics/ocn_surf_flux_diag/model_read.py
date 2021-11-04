f9236ea5b69b7d292ba9b3992f5116c320fbff94
CWHSUketo <Chia-Wei.Hsu@colostate.edu>
2021-11-04 08:27:58 -0600

    include function docstring

--- diagnostics/ocn_surf_flux_diag/model_read.py
+++ diagnostics/ocn_surf_flux_diag/model_read.py
@@ -5,7 +5,48 @@ import metpy.calc
 from metpy.units import units
 
 def regional_var(varlist, lon_lim, lat_lim, year_lim):
+    """
+    The model io for needed variables and crop based on the 
+    region and period of interest.
+    
+    Varlist needs to include atmosphere model variables
+    1. 'huss'    : Surface 2m Humidity (kg kg-1)
+    2. 'ts'      : Skin Temperature (SST for open ocean; K)
+    3. 'sfcWind' : Near-Surface Wind Speed (10 meter; m s-1)
+    4. 'psl'     : Sea Level Pressure (Pa)
+    5. 'hfls'    : Surface Upward Latent Heat Flux (W m-2 and positive upward)
+    6. 'pr'      : Precipitation (kg m-2 s-1)
+    If there are more needed model variables, user can manually 
+    add variable in the varlist.
 
+    The io function also calculate the 
+        1. surface saturated specific humidity (qsurf)
+        2. specific humidity difference between qsurf and huss (del_q)
+
+    To accomplish the calculation of qsurf
+        - ts (surface temperature)
+        - psl (sea level pressure)
+    are nesessary variables in this model io. 
+
+    Inputs:
+    =================
+    varlist (list) : a list object containing all the file locations.
+                     
+    lon_lim (list) : a list object containing the min and max value of 
+                     longitude of the desired region.
+
+    lat_lim (list) : a list object containing the min and max value of 
+                     latitude of the desired region.
+    
+    year_lim (list) : a list object containing the min and max value of 
+                     year of the desired period.
+
+
+    Outputs:
+    =================
+    ds_atm_regional (xr.Dataset) : a xarray dataset including all variables
+
+    """
 
     
     for nvar,var in enumerate(varlist):
@@ -51,7 +92,7 @@ def regional_var(varlist, lon_lim, lat_lim, year_lim):
     # unit for mixing ratio and specific humidity is kg/kg
     da_q_surf.values = q_surf.magnitude
     ds_atm_regional['qsurf'] = da_q_surf
-    ds_atm_regional['del_q'] = ds_atm_regional['qsurf']-ds_atm_regional['huss']
+    ds_atm_regional['del_q'] = 0.98*ds_atm_regional['qsurf']-ds_atm_regional['huss']
     ds_atm_regional['del_q'] = ds_atm_regional['del_q']*1e3   # kg/kg => g/kg
 
     return ds_atm_regional

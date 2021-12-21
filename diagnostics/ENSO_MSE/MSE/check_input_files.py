1b4e8dd8eb2c9ef24f4a1d5a1ab0152ef1da04c2
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2021-12-21 11:52:05 -0500

    clean up MSE/check_input_files.py

--- diagnostics/ENSO_MSE/MSE/check_input_files.py
+++ diagnostics/ENSO_MSE/MSE/check_input_files.py
@@ -1,8 +1,6 @@
 
 import os
 import sys
-### import xarray as xr 
-
 
 shared_dir = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
@@ -10,45 +8,40 @@ shared_dir = os.path.join(
 )
 sys.path.insert(0, shared_dir)
 
-###   
-###  check  the input data in COMPOSITE/model  directories 
-## 3D
-size = 12
+# check the input data in COMPOSITE/model directories
+# 3D variables
+vvar = ["zg", "ua", " va", "ta", " hus", "wap", "pr", "ts", "hfls", "hfss", "lw", "sw"]
+mode = ["ELNINO", "LANINA"]
+wkdir = os.environ["WK_DIR"]
 
-vvar  = [ "zg",  "ua", "va",  "ta", "hus", "wap", "pr",  "ts", "hfls", "hfss", "lw", "sw" ] 
-##
- 
-mode = [ "ELNINO", "LANINA" ]
+# check for missing files
+for iv in range(0, len(vvar)):
+    for n in range(0, len(mode)):
+        dirpath = os.path.join(wkdir, "COMPOSITE/model/netCDF", mode[n])
+        try:
+            os.path.exists(dirpath)
+        except IsADirectoryError:
+            print("Directory", dirpath, "does not exist")
+            sys.exit(1)
+        filevar = os.path.join(dirpath, vvar[iv] + ".nc")
+        try:
+            os.path.exists(filevar)
+        except FileExistsError:
+            print("===  MISSING INPUT FILE ", filevar)
+            print("====  EXITING ====")
+            sys.exit(1)
 
-## check for missing files 
+    filevarclim = os.path.join(wkdir, "COMPOSITE/model/netCDF", vvar[iv] + "_clim.nc")
+    try:
+        os.path.exists(filevarclim)
+    except FileExistsError:
+        print("===  MISSING INPUT FILE ", filevarclim)
+        print("====  EXITING ==== ")
+        sys.exit(1)
 
+print(" =========================================================")
+print(" ==========================================================")
+print("===  All model input files required for MSE computation found ===")
+print(" =========================================================")
+print(" ==========================================================")
 
-for iv in range(0, size):
-
-    for n in range(0, 2):
-        filevar = os.environ["WK_DIR"] + "/COMPOSITE/model/netCDF/" + mode[n] + "/" + vvar[iv] + ".nc"
-
-        if not os.path.exists(filevar):
-            print ("=============================================")
-            print ("===  MISSING INPUT FILE " + filevar  )
-            print ("====  EXITING =================== ")
-            ##raw_input("Press any key to continue")
-            sys.exit()
-
-
-    filevar = os.environ["WK_DIR"] + "/COMPOSITE/model/netCDF/" + vvar[iv] + "_clim.nc"
-
-    if not os.path.exists(filevar):
-        print ("=============================================")
-        print ("===  MISSING INPUT FILE " + filevar  )
-        print ("====  EXITING =================== ")
-        raw_input("Press any key to continue")
-        sys.exit()
-
-print  (" =========================================================")
-print  (" ==========================================================")
-print  ("===========  All model input files found =============== ")
-print  ( " =========================================================")
-print  (" ==========================================================")
-
-####

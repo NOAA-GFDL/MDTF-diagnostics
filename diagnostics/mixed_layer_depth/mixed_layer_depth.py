8b2e879fe56ef747e8d2788c3761c19e073101b0
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2022-10-18 15:15:50 -0400

    replace call to (apparently) deprecated xarray.ufuncs.isnan with np.isnan in mixed_layer_depth.py

--- diagnostics/mixed_layer_depth/mixed_layer_depth.py
+++ diagnostics/mixed_layer_depth/mixed_layer_depth.py
@@ -226,7 +226,7 @@ def computemld(fieldso, fieldthetao):
     )  # units 'meters'
 
     # set MLD to water depth where MLD is NaN
-    mld = mld.where(~xr.ufuncs.isnan(mld), bottom_depth)
+    mld = mld.where(~np.isnan(mld), bottom_depth)
 
     return mld
 

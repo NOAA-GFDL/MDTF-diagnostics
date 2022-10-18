dbee62eacc88163a5804554da3a0682690b6ef9a
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2022-10-18 14:18:42 -0400

    update python 3.7 to python 3.8 in docs, requirements, and version checking

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", 'r') as f:
 packages = find_packages()
 setup(
     name='MDTF-diagnostics',
-    version='3.0 beta 4',
+    version='3.0 beta 5',
     description='Process-oriented diagnostics for weather and climate simulations',
     license='LGPLv3',
     long_description=long_description,
@@ -38,13 +38,13 @@ setup(
             # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
             "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
     ],
     scripts=[
         'mdtf_framework.py'
     ],
     packages=packages,
-    cmdclass={ # hook for post-install commands
+    cmdclass={  # hook for post-install commands
         'develop': PostDevelopCommand,
         'install': PostInstallCommand
     }

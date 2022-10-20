5c0db2f4068c8135da33b432de062cb62be8adbe
Jess <20195932+wrongkindofdoctor@users.noreply.github.com>
2022-10-20 07:47:21 -0400

    bump python to version 3.8 in conda env config files and mdtf_test yaml (#415)
    * bump python to version 3.8 in conda env config files and mdtf_test yaml
    fix typo in conda_init.sh
    bump other package versions in yamls to be consistent with base env
    
    * update python 3.7 to python 3.8 in docs, requirements, and version checking
    
    * replace call to (apparently) deprecated xarray.ufuncs.isnan with np.isnan in mixed_layer_depth.py

--- src/install.py
+++ src/install.py
@@ -5,8 +5,8 @@ conda environments and supporting data.
 
 import sys
 # do version check before importing other stuff
-if sys.version_info[0] != 3 or sys.version_info[1] < 7:
-    sys.exit("ERROR: MDTF currently only supports python >= 3.7.*. Please check "
+if sys.version_info[0] != 3 or sys.version_info[1] < 8:
+    sys.exit("ERROR: MDTF currently only supports python >= 3.8.*. Please check "
     "which version is on your $PATH (e.g. with `which python`.)\n"
     f"Attempted to run with following python version:\n{sys.version}")
 # passed; continue with imports

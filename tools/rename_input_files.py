6d59009ae1972c80a203ce84c771b45c78872cdf
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2022-12-06 16:15:41 -0500

    add functions to make new file names and link files to rename_input_files.py

--- tools/rename_input_files.py
+++ tools/rename_input_files.py
@@ -18,10 +18,11 @@
 import os
 import yaml
 import click
+from pathlib import Path
 
 @click.command()
 @click.option('--config_file', type=str, help='Path to the MDTF-diagnostics runtime config file')
-def cli_parser(config_file: str):
+def main(config_file: str):
     """A developer tool to rename netCDF input files to match the MDTF-diagnostics Local_file format"""
     assert (os.path.isfile(config_file)), "File not Found. Check Path to config file for typos."
     with open(config_file, 'r') as stream:
@@ -31,6 +32,25 @@ def cli_parser(config_file: str):
         except yaml.YAMLError as exc:
             print(exc)
 
+    casename = case_info['CASENAME']
+    in_path = case_info['origFilePath']
+    out_path = case_info['outputFilePath']
+    append_new_filenames(case_info['files'], casename)
+    link_files(in_path, out_path, case_info['files'])
+
+
+def append_new_filenames(file_list=list, casename=str):
+    for f in file_list:
+        print(f)
+        f['new_name'] = '.'.join([casename, f['var'], f['freq'], 'nc'])
+
+def link_files(input_dir_path=str, output_dir_path=str, file_list=list):
+    for f in file_list:
+        f['inpath'] = os.path.join(input_dir_path,f['name'])
+        assert os.path.isfile(f['inpath']), f['inpath']+" not found. Check file name and path for errors."
+        f['outpath'] = os.path.join(output_dir_path)
+        Path(f['outpath']).mkdir(parents=True, exist_ok=True)
+
 
 if __name__ == '__main__':
-    cli_parser(prog_name='Rename Input Files')
+    main(prog_name='Rename Input Files')

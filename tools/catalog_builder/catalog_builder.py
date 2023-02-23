e96db60cc035caecd01d59d71084d745c4b156b1
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-02-23 16:48:31 -0500

    added wrappers for ecgtools build and save methods added ref to SO post with classmaker code need to refine wrappers for ecgtools functions

--- tools/catalog_builder/catalog_builder.py
+++ tools/catalog_builder/catalog_builder.py
@@ -17,18 +17,16 @@
 # Output: esm-intake catalog in csv format with the file locations and metadata required
 # for the preprocessor and the MDTF-diagnostics framework
 
-
 import os
-from abc import ABC, abstractmethod
-import shutil
-import sys
-import yaml
-from pathlib import Path
-import dask
 import intake
 from ecgtools import Builder
 from ecgtools.parsers.cesm import parse_cesm_timeseries
+from ecgtools.parsers.cmip import parse_cmip_timeseries
 import click
+
+# The ClassMaker is cribbed from SO
+# https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
+# It allows
 
 
 class ClassMaker:
@@ -51,36 +49,78 @@ class ClassMaker:
 catalog_class = ClassMaker()
 
 
+class CatalogBase(object):
+    """Catalog base class\n
+    """
+
+    def __init__(self):
+        self.joblib_parallel_kwargs = {'n_jobs': -1}  # default parallel jobs
+        self.depth = 1  # default directory depth to traverse when searching data_paths
+        self.cb = None
+
+    def cat_builder(self, data_paths: list,
+                    exclude_patterns=None):
+        if exclude_patterns is None:
+            exclude_patterns = ["DO_NOT_USE"]
+        self.cb = Builder(paths=data_paths,
+                          depth=self.depth,
+                    # Exclude the following directories
+                    exclude_patterns=exclude_patterns,
+                    # Number of jobs to execute - should be equal to # threads you are using
+                    joblib_parallel_kwargs=self.joblib_parallel_kwargs,
+                          )
+
+    def cat_save(self, output_dir: str, output_filename: str):
+        return self.save(
+            os.path.join(output_dir, output_filename),
+            # Column name including filepath
+            path_column_name='path',
+            # Column name including variables
+            variable_column_name='variable',
+            # Data file format - could be netcdf or zarr (in this case, netcdf)
+            data_format="netcdf",
+            # Which attributes to groupby when reading in variables using intake-esm
+            groupby_attrs=["component", "stream", "case"],
+            # Aggregations which are fed into xarray when reading in data using intake
+            aggregations=[
+                {'type': 'union', 'attribute_name': 'variable'},
+                {
+                    "type": "join_existing",
+                    "attribute_name": "time_range",
+                    "options": {"dim": "time", "coords": "minimal", "compat": "override"},
+                },
+            ],
+        )
+
+
 @catalog_class.maker
-class CatalogCMIP(CatalogBase, ABC):
+class CatalogCMIP(CatalogBase):
     """Class to generate CMIP data catalogs\n
     """
 
-    def cat_builder(self, *args, **kwargs):
+    def build(self):
+        return self.build(parse_cmip_timeseries)
+
+
+@catalog_class.maker
+class CatalogGFDL(CatalogBase):
+    """Class to generate GFDL data catalogs\n
+    """
+
+    def build(self):
         pass
+        # TODO create parse_gfdl_timeseries module and
+        # submit PR to ecgtools
+        # return self.build(parse_gfdl_timeseries)
 
-    def drs(self):
-        pass
 
-    class CatalogGFDL(CatalogBase, ABC):
-        """Class to generate GFDL data catalogs\n
-        """
+@catalog_class.maker
+class CatalogCESM(CatalogBase):
+    """Class to generate CESM data catalogs\n
+    """
 
-        def cat_builder(self, *args, **kwargs):
-            pass
-
-        def drs(self):
-            pass
-
-    class CatalogCESM(CatalogBase, ABC):
-        """Class to generate CESM data catalogs\n
-        """
-
-        def cat_builder(self, *args, **kwargs):
-            pass
-
-        def drs(self):
-            pass
+    def build(self):
+        return self.build(parse_cesm_timeseries)
 
 
 @click.command()
@@ -108,38 +148,15 @@ def main(data_paths: list, convention: str, output_dir: str, output_filename: st
         except FileNotFoundError:
             print("{p} not found. Check data_paths for typos.")
 
-    cat_builder = Builder(
-        # Archive directory with GFDL pp model output
-        paths=data_paths,
-        # Depth of 4 since we are traversing the component/ts/freq/chunk directory
-        depth=4,
-        # Exclude the following directories
-        exclude_patterns=["*/av/*", "*DO_NOT_USE"],
-        # Number of jobs to execute - should be equal to # threads you are using
-        joblib_parallel_kwargs={'n_jobs': -1},
-    )
-    cat_builder  # instantiate a builder object for the pp directory
+    # instantiate the builder class instance for the specified convention
+    cat_builder = catalog_class["Catalog" + convention.upper()]
+    # initialize the esm-intake builder object
+    cat_builder.builder(data_paths=data_paths,
+                        exclude_patterns=exclude_patterns)
 
-    cat_builder = cat_builder.build(parse_cesm_timeseries)
+
+    cat_builder = cat_builder.build()
     # save the catalog
-    cat_builder.save(
-        os.path.join(output_dir, output_filename),
-        # Column name including filepath
-        path_column_name='path',
-        # Column name including variables
-        variable_column_name='variable',
-        # Data file format - could be netcdf or zarr (in this case, netcdf)
-        data_format="netcdf",
-        # Which attributes to groupby when reading in variables using intake-esm
-        groupby_attrs=["component", "stream", "case"],
-        # Aggregations which are fed into xarray when reading in data using intake
-        aggregations=[
-            {'type': 'union', 'attribute_name': 'variable'},
-            {
-                "type": "join_existing",
-                "attribute_name": "time_range",
-                "options": {"dim": "time", "coords": "minimal", "compat": "override"},
-            },
-        ],
-    )
+    cat_builder.save(output_dir, output_filename)
+
 

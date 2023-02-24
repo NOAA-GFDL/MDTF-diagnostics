6893ff0fec50f1de041853db67389646ea1c1449
Jessica.Liptak <jessica.liptak@noaa.gov>
2023-02-24 12:43:21 -0500

    created config argument and yaml parser to read intake from a yaml configuration file refined the cat_builder function having issues calling the ecgtools cmip parser; may need to follow directions to create catalog from scratch w/o ecgtools

--- tools/catalog_builder/catalog_builder.py
+++ tools/catalog_builder/catalog_builder.py
@@ -1,11 +1,6 @@
 # ======================================================================
 # NOAA Model Diagnostics Task Force (MDTF)
-#
-# Rename input files
-#
 # ======================================================================
-# Usage
-#
 # USAGE: Build esm-intake catalogs for files stored using CMIP6, CESM, or GFDL
 # DRSs
 #
@@ -19,15 +14,19 @@
 
 import os
 import intake
+import pathlib
+import yaml
 from ecgtools import Builder
 from ecgtools.parsers.cesm import parse_cesm_timeseries
-from ecgtools.parsers.cmip import parse_cmip_timeseries
+from ecgtools.parsers.cmip import parse_cmip6
 import click
 
 # The ClassMaker is cribbed from SO
 # https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
-# It allows
-
+# Classmaker and the @catalog_class.maker decorator allow class instantiation from
+# strings. The main block can simply call the desired class using the convention
+# argument instead of messy if/then/else blocks. Yes, both work, but I wanted
+# to try something that, if it is not more "Pythonic", is more extensible
 
 class ClassMaker:
     def __init__(self):
@@ -55,34 +54,9 @@ class CatalogBase(object):
 
     def __init__(self):
         self.joblib_parallel_kwargs = {'n_jobs': -1}  # default parallel jobs
-        self.depth = 1  # default directory depth to traverse when searching data_paths
-        self.cb = None
-
-    def cat_builder(self, data_paths: list,
-                    exclude_patterns=None):
-        if exclude_patterns is None:
-            exclude_patterns = ["DO_NOT_USE"]
-        self.cb = Builder(paths=data_paths,
-                          depth=self.depth,
-                    # Exclude the following directories
-                    exclude_patterns=exclude_patterns,
-                    # Number of jobs to execute - should be equal to # threads you are using
-                    joblib_parallel_kwargs=self.joblib_parallel_kwargs,
-                          )
-
-    def cat_save(self, output_dir: str, output_filename: str):
-        return self.save(
-            os.path.join(output_dir, output_filename),
-            # Column name including filepath
-            path_column_name='path',
-            # Column name including variables
-            variable_column_name='variable',
-            # Data file format - could be netcdf or zarr (in this case, netcdf)
-            data_format="netcdf",
-            # Which attributes to groupby when reading in variables using intake-esm
-            groupby_attrs=["component", "stream", "case"],
-            # Aggregations which are fed into xarray when reading in data using intake
-            aggregations=[
+        self.groupby_attrs = ["component", "stream", "case"] # attributes to group by when reading
+        # in variables using intake-esm
+        self.xarray_aggregations = [
                 {'type': 'union', 'attribute_name': 'variable'},
                 {
                     "type": "join_existing",
@@ -90,6 +64,39 @@ class CatalogBase(object):
                     "options": {"dim": "time", "coords": "minimal", "compat": "override"},
                 },
             ],
+        self.data_format = "netcdf" # netcdf or zarr
+        self.cb = None
+
+    def cat_builder(self, data_paths: list,
+                    exclude_patterns=None,
+                    dir_depth=1
+                    ):
+        if exclude_patterns is None:
+            exclude_patterns = ["DO_NOT_USE"]
+        self.cb = Builder(paths=data_paths,
+                          depth=dir_depth,
+                          exclude_patterns=exclude_patterns,  # Exclude the following directories
+                          joblib_parallel_kwargs=self.joblib_parallel_kwargs,  # Number of jobs to execute -
+                          # should be equal to # threads you are using
+                          extension='.nc'  # extension of target files
+                          )
+        print('ddd')
+
+    def cat_save(self, output_dir: str,
+                 output_filename: str
+                 ):
+        self.cb.save(
+            os.path.join(output_dir, output_filename),
+            # Column name including filepath
+            path_column_name='path',
+            # Column name including variables
+            variable_column_name='variable',
+            # Data file format - could be netcdf or zarr (in this case, netcdf)
+            data_format=self.data_format,
+            # Which attributes to group by when reading in variables using intake-esm
+            groupby_attrs=self.groupby_attrs,
+            # Aggregations which are fed into xarray when reading in data using intake
+            aggregations=self.xarray_aggregations
         )
 
 
@@ -98,8 +105,13 @@ class CatalogCMIP(CatalogBase):
     """Class to generate CMIP data catalogs\n
     """
 
-    def build(self):
-        return self.build(parse_cmip_timeseries)
+    def call_build(self, file_parse_method=None):
+        if file_parse_method is None:
+            file_parse_method = parse_cmip6
+        # see https://github.com/ncar-xdev/ecgtools/blob/main/ecgtools/parsers/cmip6.py
+        # for more parsing methods
+        self.cb = self.cb.build(file_parse_method)
+        print('RRR')
 
 
 @catalog_class.maker
@@ -107,7 +119,7 @@ class CatalogGFDL(CatalogBase):
     """Class to generate GFDL data catalogs\n
     """
 
-    def build(self):
+    def call_build(self):
         pass
         # TODO create parse_gfdl_timeseries module and
         # submit PR to ecgtools
@@ -118,45 +130,48 @@ class CatalogGFDL(CatalogBase):
 class CatalogCESM(CatalogBase):
     """Class to generate CESM data catalogs\n
     """
+    def call_build(self, file_parse_method=None):
+        if file_parse_method is None:
+            file_parse_method = parse_cesm_timeseries
+        # see https://github.com/ncar-xdev/ecgtools/blob/main/ecgtools/parsers/cesm.py
+        # for more parsing methods
+        self.cb.build(file_parse_method)
 
-    def build(self):
-        return self.build(parse_cesm_timeseries)
+def load_config(config):
+    if os.path.exists(config):
+        with open(config, 'r') as f:
+            return yaml.safe_load(f.read())
 
 
+@click.option('--config',
+              type=click.Path(),
+              help='Path to config file'
+              )
 @click.command()
-@click.option('--data_paths',
-              type=list,
-              help='List of path(s) to the root directory(ies) with the desired files')
-@click.option('--convention',
-              default='cmip',
-              type=click.Choice(['cmip', 'gfdl', 'cesm', 'ncar']),
-              help='DRS convention')
-@click.option('--output_dir',
-              default='./',
-              help='Directory where catalog csv file will be written (default current working directory')
-@click.option('--output_filename',
-              default='catalog.csv',
-              help='Name of the data catalog csv file')
-@click.option('--exclude_patterns',
-              type=list,
-              help='List of directories or wildcard patterns to exclude from catalog')
-def main(data_paths: list, convention: str, output_dir: str, output_filename: str, exclude_patterns:list=[]):
+def main(config: str):
     """A tool to generate intake-esm catalogs of datasets to preprocess for use with the MDTF-diagnostics package"""
-    for p in data_paths:
+    conf = load_config(config)
+    for p in conf['data_root_dirs']:
         try:
             os.path.isdir(p)
         except FileNotFoundError:
-            print("{p} not found. Check data_paths for typos.")
+            print("{p} not found. Check data_root_dirs for typos.")
 
     # instantiate the builder class instance for the specified convention
-    cat_builder = catalog_class["Catalog" + convention.upper()]
-    # initialize the esm-intake builder object
-    cat_builder.builder(data_paths=data_paths,
-                        exclude_patterns=exclude_patterns)
-
-
-    cat_builder = cat_builder.build()
+    cat_cls = catalog_class["Catalog" + conf['convention'].upper()]
+    # initialize the catalog object
+    cat_obj = cat_cls()
+    # instantiate the esm catalog builder
+    cat_obj.cat_builder(data_paths=conf['data_root_dirs'],
+                        exclude_patterns=conf['exclude_patterns'],
+                        dir_depth=conf['dir_depth']
+                        )
+    # build the catalog
+    cat_obj.call_build()
     # save the catalog
-    cat_builder.save(output_dir, output_filename)
+    cat_obj.save(output_dir=conf['output_dir'],
+                 output_filename=conf['output_filename']
+                 )
 
-
+if __name__ == '__main__':
+    main(prog_name='ESM-Intake Catalog Maker')

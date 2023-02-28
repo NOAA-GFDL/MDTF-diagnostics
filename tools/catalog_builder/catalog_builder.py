fdb0eb4cc44d286571800e604f8c1ee6f823bb2d
Jessica.Liptak <jessica.liptak@noaa.gov>
2023-02-28 12:49:58 -0500

    add placeholder custom parser for uda data fix calls to parser commands--need to specify with file_parse_method argument in latest version per open issue script currently seems to hang so need to refine threads and optimize directory depth

--- tools/catalog_builder/catalog_builder.py
+++ tools/catalog_builder/catalog_builder.py
@@ -12,14 +12,20 @@
 # Output: esm-intake catalog in csv format with the file locations and metadata required
 # for the preprocessor and the MDTF-diagnostics framework
 
-import os
-import intake
-import pathlib
-import yaml
-from ecgtools import Builder
-from ecgtools.parsers.cesm import parse_cesm_timeseries
-from ecgtools.parsers.cmip import parse_cmip6
 import click
+import glob
+import intake
+import os
+import pathlib
+import traceback
+import xarray as xr
+import yaml
+from datetime import datetime
+from ecgtools import Builder
+from ecgtools.builder import INVALID_ASSET, TRACEBACK
+from ecgtools.parsers.cesm import parse_cesm_timeseries
+from ecgtools.parsers import parse_cmip6
+
 
 # The ClassMaker is cribbed from SO
 # https://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
@@ -47,6 +53,58 @@ class ClassMaker:
 # instantiate the class maker
 catalog_class = ClassMaker()
 
+# custom parser for data stored on GFDL uda
+# TODO: move to separate module and submit PR to ecg-tools repo
+def parse_gfdl_uda(file_name: str):
+    #files = sorted(glob.glob(os.path.join(root_dir,'*/*/*/*/*/*/*.nc')))
+    #file = pathlib.Path(files[0])
+    file = pathlib.Path(file_name[0])
+    info = dict()
+
+    try:
+        # isolate file from rest of path
+        stem = file.stem
+        # split the file name into components based on _
+        split = stem.split('_')
+        variable_id = split[0]
+        table_id = split[1]
+        source_id = split[2]
+        experiment_id = split[3]
+        variant_label = split[4]
+        grid_label = split[5]
+        date_range = split[6]
+        if 'mon' in table_id.lower():
+            output_frequency = 'mon'
+        elif 'day' in table_id.lower():
+            output_frequency = 'day'
+        elif '6hr' in table_id.lower():
+            output_frequency = '6hr'
+        elif 'subhr' in table_id.lower():
+            output_frequency = 'subhr'
+
+        with xr.open_dataset(file, chunks={}, decode_times=False) as ds:
+            variable_list = [var for var in ds if 'standard_name' in ds[var].attrs]
+
+            info = {
+                'sample_dataset': source_id,
+                'variable_id': variable_id,
+                'table_id': table_id,
+                'source_id': source_id,
+                'experiment_id': experiment_id,
+                'variant_label': variant_label,
+                'grid_label': grid_label,
+                'date_range': date_range,
+                'frequency': output_frequency,
+                'variable': variable_list[0],
+                'file_name': stem,
+                'path': str(file),
+            }
+
+        return info
+
+    except Exception:
+        return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
+
 
 class CatalogBase(object):
     """Catalog base class\n
@@ -54,7 +112,7 @@ class CatalogBase(object):
 
     def __init__(self):
         self.joblib_parallel_kwargs = {'n_jobs': -1}  # default parallel jobs
-        self.groupby_attrs = ["component", "stream", "case"] # attributes to group by when reading
+        self.groupby_attrs = ["component", "stream", "case"]  # attributes to group by when reading
         # in variables using intake-esm
         self.xarray_aggregations = [
                 {'type': 'union', 'attribute_name': 'variable'},
@@ -80,7 +138,6 @@ class CatalogBase(object):
                           # should be equal to # threads you are using
                           extension='.nc'  # extension of target files
                           )
-        print('ddd')
 
     def cat_save(self, output_dir: str,
                  output_filename: str
@@ -105,12 +162,38 @@ class CatalogCMIP(CatalogBase):
     """Class to generate CMIP data catalogs\n
     """
 
+    def __init__(self):
+        super().__init__()
+        self.groupby_attrs = [
+            'activity_id',
+            'institution_id',
+            'source_id',
+            'experiment_id',
+            'table_id',
+            'grid_label'
+        ],
+        self.xarray_aggregations = [
+            {'type': 'union', 'attribute_name': 'variable_id'},
+            {
+                'type': 'join_existing',
+                'attribute_name': 'date_range',
+                'options': {'dim': 'time', 'coords': 'minimal', 'compat': 'override'},
+            }
+        ]
+
+    def cat_builder(self, data_paths: list,
+                    exclude_patterns=None,
+                    dir_depth=1
+                    ):
+
+        self.cb = Builder(paths=data_paths, depth=dir_depth, njobs=4)
+
     def call_build(self, file_parse_method=None):
         if file_parse_method is None:
             file_parse_method = parse_cmip6
         # see https://github.com/ncar-xdev/ecgtools/blob/main/ecgtools/parsers/cmip6.py
         # for more parsing methods
-        self.cb = self.cb.build(file_parse_method)
+        self.cb = self.cb.build(parsing_func=file_parse_method)
         print('RRR')
 
 
@@ -156,6 +239,8 @@ def main(config: str):
             os.path.isdir(p)
         except FileNotFoundError:
             print("{p} not found. Check data_root_dirs for typos.")
+        data_obj = parse_gfdl_uda(p)  # debug custom parser
+
 
     # instantiate the builder class instance for the specified convention
     cat_cls = catalog_class["Catalog" + conf['convention'].upper()]
@@ -167,8 +252,10 @@ def main(config: str):
                         dir_depth=conf['dir_depth']
                         )
     # build the catalog
+    print('Building the catalog')
     cat_obj.call_build()
     # save the catalog
+    print('Saving catalog to', conf['output_filename'])
     cat_obj.save(output_dir=conf['output_dir'],
                  output_filename=conf['output_filename']
                  )

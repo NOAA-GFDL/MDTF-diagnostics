46dff5a715e1bca4c9263289047e6f4829222ef2
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-03-21 15:13:19 -0400

    added lines to define current branch env variable and push updates to it

--- data/cmip6-cmor-tables/scripts/CMIP6_CV_cron_github.sh
+++ data/cmip6-cmor-tables/scripts/CMIP6_CV_cron_github.sh
@@ -11,6 +11,8 @@ mv ${REPO_PATH}/scripts/CMIP6_CV.json  ${REPO_PATH}/Tables
 msg="cron: update CMIP6_CV -- "`date +%Y-%m-%dT%H:%M`
 echo $msg
 cd ../../
+export CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 git commit -am "$msg"
-git push
+echo "Pushing updates to branch ${CURRENT_BRANCH}"
+git push -u origin ${CURRENT_BRANCH}
 git remote rm cmor_tables

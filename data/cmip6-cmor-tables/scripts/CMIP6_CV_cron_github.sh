ab850abfc265dd63b9dcc5b6bae3b9e230dff839
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-03-21 15:02:35 -0400

    mod CMIP6_CV_cron_github.sh to update repo in MDTF-diagnostics subdirectory

--- data/cmip6-cmor-tables/scripts/CMIP6_CV_cron_github.sh
+++ data/cmip6-cmor-tables/scripts/CMIP6_CV_cron_github.sh
@@ -2,11 +2,14 @@
 export REPO_PATH=${1:-"${HOME}/cmip6-cmor-tables"}
 echo ${REPO_PATH}
 cd ${REPO_PATH}
-git pull origin master
+git remote add cmor_tables 
+git fetch -u cmor_tables
+git pull
 cd scripts
-python createCMIP6CV.py
+python3 createCMIP6CV.py
 mv ${REPO_PATH}/scripts/CMIP6_CV.json  ${REPO_PATH}/Tables
 msg="cron: update CMIP6_CV -- "`date +%Y-%m-%dT%H:%M`
 echo $msg
+cd ../../
 git commit -am "$msg"
 git push

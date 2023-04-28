03bdfa77082a8ef6bfefceabfe29309e35f27cb2
Jessica.Liptak <jessica.liptak@noaa.gov>
2023-04-28 16:29:27 -0400

    replace catalog builder csh batch script with bash script, add catalogs for IPSL-CM5A2-INCA-historical run

--- tools/catalog_builder/examples/templates/catalog_builder_slurm.sh
+++ tools/catalog_builder/examples/templates/catalog_builder_slurm.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env tcsh
+#!/usr/bin/env bash
 # Slurm batch submission script template for the MDTF-diagnostics ESM-Intake catalog builder:
 # Usage:
 # > cd /nbhome/[USERNAME]/mdtf/MDTF-diagnostics/tools/catalog_builder
-# > sbatch examples/templates/catalog_builder_slurm.csh --config examples/templates/example_builder_config.yml
+# > sbatch examples/templates/catalog_builder_slurm.sh --config examples/templates/example_builder_config.yml
 #SBATCH --job-name=esm_cat_builder
-#SBATCH --chdir=/nbhome/[USERNAME]
-#SBATCH --output=/nbhome/[USERNAME]/logs/slurm_%x.%A_%a.out
-#SBATCH --error=/nbhome/[USERNAME]/logs/slurm_%x.%A_%a.err
+#SBATCH --chdir=/nbhome/jml
+#SBATCH --output=/nbhome/jml/logs/slurm_%x.%A_%a.out
+#SBATCH --error=/nbhome/jml/logs/slurm_%x.%A_%a.err
 #SBATCH --time=1:00:00
 #SBATCH --ntasks=8
 #SBATCH --constraint=bigmem
 #
-local_repo="/nbhome/[USERNAME]/mdtf/MDTF-diagnostics/tools/catalog_builder"
+local_repo="/nbhome/jml/catalog_builder"
 _mdtf="/home/oar.gfdl.mdtf/mdtf/MDTF-diagnostics"
 source "${_mdtf}/src/conda/conda_init.sh" -q "/home/oar.gfdl.mdtf/miniconda3"
 conda activate _MDTF_base

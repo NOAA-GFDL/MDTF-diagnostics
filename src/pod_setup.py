82de1ec35e08aa98db4c00949f4ca042b8df9ac2
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-07-05 16:37:45 -0400

    refine pod_setup methods

--- src/pod_setup.py
+++ src/pod_setup.py
@@ -7,7 +7,7 @@ import io
 from pathlib import Path
 from typing import Type
 
-from src import cli, util, data_sources, translation, varlistentry_util, varlist_util
+from src import cli, util, diagnostic, data_sources, translation, varlistentry_util, varlist_util
 import intake_esm
 import dataclasses as dc
 
@@ -33,9 +33,10 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
     # _id
     # _parent: object
     # status: ObjectStatus
-    pod_dims = util.NameSpace
-    pod_vars = util.NameSpace
-    pod_settings = util.NameSpace
+    pod_dims = dict()
+    pod_data = dict()
+    pod_vars = dict()
+    pod_settings = dict()
     cases = util.NameSpace
 
     MODEL_DATA_DIR = dict()
@@ -80,6 +81,11 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
                 self.log_file.write(self.format_log(children=False))
             self.log_file.close()
 
+    def iter_case_names(self):
+        """Iterator returning :c
+        """
+        yield self.cases.keys()
+
     def parse_pod_settings_file(self, code_root: str) -> util.NameSpace:
         """Parse the POD settings file"""
         settings_file_query = Path(code_root, 'diagnostics', self.name).glob('*settings.*')
@@ -88,19 +94,35 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
         settings_dict = cli.parse_config_file(settings_file_path)
         return util.NameSpace.fromDict({k: settings_dict[k] for k in settings_dict.keys()})
 
+    def verify_pod_settings(self):
+        """Verify that the POD settings file has the required entries"""
+        required_settings = {"driver": str, "long_name": "", "convention": "",
+                             "runtime_requirements": list}
+        value = []
+        try:
+            value = [i for i in required_settings if i in self.pod_settings
+                     and isinstance(self.pod_settings[i], type(required_settings[i]))]
+        except Exception as exc:
+            raise util.PodConfigError("Caught Exception: required setting %s not in pod setting file %s", value[0])\
+                from exc
+
     def _get_pod_settings(self, pod_settings_dict: util.NameSpace):
         self.pod_settings = util.NameSpace.toDict(pod_settings_dict.settings)
+
+    def _get_pod_data(self, pod_settings_dict: util.NameSpace):
+        self.pod_data = util.NameSpace.toDict(pod_settings_dict.data)
 
     def _get_pod_dims(self, pod_settings_dict: util.NameSpace):
         self.pod_dims = util.NameSpace.toDict(pod_settings_dict.dims)
 
     def _get_pod_vars(self, pod_settings_dict: util.NameSpace):
-        self.pod_vars = util.NameSpace.toDict(pod_settings_dict.vars)
+        self.pod_vars = util.NameSpace.toDict(pod_settings_dict.varlist)
 
-    def get_pod_data_subset(self, catalog_path, case_list):
+    def get_pod_data_subset(self, catalog_path: str, case_data_source):
         cat = intake.open_esm_datastore(catalog_path)
         # filter catalog by desired variable and output frequency
-        tas_subset = cat.search(variable_id=tas_var, frequency="day")
+        tas_subset = cat.search(variable_id=case_data_source.varlist.iter_vars(),
+                                frequency=self.pod_data['freq'])
 
     def query_files_in_time_range(self, startdate, enddate):
         pass
@@ -113,31 +135,16 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
 
         Raises: :class:`~util.PodRuntimeError` if driver script can't be found.
         """
-        if not self.pod_settings.driver:
-            self.log.warning("No valid driver script found for %s.", self.full_name)
-            # try to find one anyway
-            script_names = [self.name, "driver"]
-            file_names = [f"{script}{ext}" for script in script_names
-                          for ext in self._interpreters.keys()]
-            for f in file_names:
-                path_ = os.path.join(self.POD_CODE_DIR, f)
-                if os.path.exists(path_):
-                    self.log.debug("Setting driver script for %s to '%s'.",
-                                   self.full_name, f)
-                    self.driver = path_
-                    break  # go with the first one found
+        self.driver = os.path.join(self.paths.POD_CODE_DIR, self.pod_settings["driver"])
         if not self.driver:
             raise util.PodRuntimeError((f"No driver script found in "
-                                        f"{self.POD_CODE_DIR}. Specify 'driver' in settings.jsonc."),
+                                        f"{self.paths.POD_CODE_DIR}. Specify 'driver' in settings.jsonc."),
                                        self)
-
         if not os.path.isabs(self.driver):  # expand relative path
-            self.driver = os.path.join(self.POD_CODE_DIR, self.driver)
-        if not os.path.exists(self.driver):
-            raise util.PodRuntimeError(
-                f"Unable to locate driver script '{self.driver}'.",
-                self
-            )
+            self.driver = os.path.join(self.paths.POD_CODE_DIR, self.driver)
+
+        self.log.debug("Setting driver script for %s to '%s'.",
+                       self.full_name, self.driver)
 
     def set_interpreter(self):
         """Determine what executable should be used to run the driver script.
@@ -167,25 +174,26 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
         pod_input = self.parse_pod_settings_file(runtime_config.code_root)
         self._get_pod_settings(pod_input)
         self._get_pod_vars(pod_input)
+        self._get_pod_data(pod_input)
         self._get_pod_dims(pod_input)
-
+        self.verify_pod_settings()
+        self.set_interpreter()
         # run the PODs on data that has already been preprocessed
         # PODs will ingest input directly from catalog that (should) contain
         # the information for the saved preprocessed files, and a pre-existing case_env file
         if runtime_config.persist_data:
             pass
         elif runtime_config.run_pp:
-
             for case_name, case_dict in runtime_config.case_list.items():
                 # instantiate the data_source class instance for the specified convention
                 self.cases.case_name = data_sources.data_source[case_dict.convention.upper() +
                                                                 "DataSource"](case_dict, parent=self)
 
                 #util.NameSpace.fromDict({k: case_dict[k] for k in case_dict.keys()})
-                if self.pod_settings.convention != case_dict.convention:
+                if self.pod_settings['convention'] != case_dict.convention:
                     # translate variable(s) to user_specified standard if necessary
 
-                    self.cases.case_name.varlist = Varlist.from_struct(case_dict)
+                    self.cases.case_name.varlist = diagnostic.Varlist.from_struct(self)
                 else:
                     pass
 
@@ -201,91 +209,27 @@ class PodObject(util.PODLoggerMixin, util.MDTFObjectBase, PodBaseClass, ABC):
         # ref for dict comparison
         # https://stackoverflow.com/questions/20578798/python-find-matching-values-in-nested-dictionary
 
-        cat_subset = get_pod_data_subset(runtime_config.CATALOG_PATH, runtime_config.case_list)
+        cat_subset = self.get_pod_data_subset(runtime_config.CATALOG_PATH, runtime_config.case_list)
 
-        self.setup_var(v, case_dict.attrs.date_range, case_name)
+        #self.setup_var(v, case_dict.attrs.date_range, case_name)
 
         # preprocessor will edit case varlist alternates, depending on enabled functions
         # self is the Mul
-        self.preprocessor = self._PreprocessorClass(self)
+        #self.preprocessor = self._PreprocessorClass(self)
         # self=MulirunDiagnostic instance, and is passed as data_mgr parm to access
         # cases
-        self.preprocessor.edit_request(self)
+        #self.preprocessor.edit_request(self)
 
-        for case_name, case_dict in self.cases.items():
-            for v in case_dict.iter_children():
+        for case_name in self.cases.:
+            for v in case.iter_children():
                 # deactivate failed variables, now that alternates are fully
                 # specified
                 if v.last_exception is not None and not v.failed:
                     v.deactivate(v.last_exception, level=logging.WARNING)
-            if case_dict.status == util.ObjectStatus.NOTSET and \
-                    any(v.status == util.ObjectStatus.ACTIVE for v in case_dict.iter_children()):
-                case_dict.status = util.ObjectStatus.ACTIVE
+            if case.status == util.ObjectStatus.NOTSET and \
+                    any(v.status == util.ObjectStatus.ACTIVE for v in case.iter_children()):
+                case.status = util.ObjectStatus.ACTIVE
         # set MultirunDiagnostic object status to Active if all case statuses are Active
         if self.status == util.ObjectStatus.NOTSET and \
-                all(case_dict.status == util.ObjectStatus.ACTIVE for case_name, case_dict in self.cases.items()):
+                all(case.status == util.ObjectStatus.ACTIVE for case in self.cases):
             self.status = util.ObjectStatus.ACTIVE
-
-    def setup_var(self, v, date_range: util.DateRange, case_name: str):
-        """Update VarlistEntry fields "v" with information that only becomes
-        available after DataManager and Diagnostic have been configured (ie,
-        only known at runtime, not from settings.jsonc.)
-
-        Could arguably be moved into VarlistEntry's init, at the cost of
-        dependency inversion.
-        """
-        translate = translation.VariableTranslator().get_convention(self.convention)
-        if v.T is not None:
-            v.change_coord(
-                'T',
-                new_class={
-                    'self': varlist_util.VarlistTimeCoordinate,
-                    'range': util.DateRange,
-                    'frequency': util.DateFrequency
-                },
-                range=date_range,
-                calendar=util.NOTSET,
-                units=util.NOTSET
-            )
-
-        v.dest_path = self.variable_dest_path(v, case_name)
-        try:
-            trans_v = translate.translate(v)
-            v.translation = trans_v
-            # copy preferred gfdl post-processing component during translation
-            if hasattr(trans_v, "component"):
-                v.component = trans_v.component
-            if hasattr(trans_v,"rename_coords"):
-                v.rename_coords = trans_v.rename_coords
-        except KeyError as exc:
-            # can happen in normal operation (eg. precip flux vs. rate)
-            chained_exc = util.PodConfigEvent((f"Deactivating {v.full_name} for multirun case {case_name} due to "
-                                               f"variable name translation: {str(exc)}."))
-            # store but don't deactivate, because preprocessor.edit_request()
-            # may supply alternate variables
-            v.log.store_exception(chained_exc)
-        except Exception as exc:
-            chained_exc = util.chain_exc(exc, f"translating name of {v.full_name} for multirun case {case_name}.",
-                                         util.PodConfigError)
-            # store but don't deactivate, because preprocessor.edit_request()
-            # may supply alternate variables
-            v.log.store_exception(chained_exc)
-
-        v.stage = varlistentry_util.VarlistEntryStage.INITED
-
-    def variable_dest_path(self, var, case_name):
-        """Returns the absolute path of the POD's preprocessed, local copy of
-        the file containing the requested dataset. Files not following this
-        convention won't be found by the POD.
-        """
-        # TODO add option for file(s) with just the var name in a designated directory
-        # Would involve a regex search for the variable name for a single file, or match
-        # all files in a specified file list (txt file, json, yaml)
-
-        if var.is_static:
-            f_name = f"{case_name}.{var.name}.static.nc"
-            return os.path.join(self.MODEL_WK_DIR[case_name], f_name)
-        else:
-            freq = var.T.frequency.format_local()
-            f_name = f"{case_name}.{var.name}.{freq}.nc"
-            return os.path.join(self.MODEL_WK_DIR[case_name], freq, f_name)

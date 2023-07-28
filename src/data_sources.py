e5bebb4ddd62e11a709e1bab1551891cb386b3e0
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-07-28 14:33:24 -0400

    add type hints and placeholder methods to DataSourceBase class

--- src/data_sources.py
+++ src/data_sources.py
@@ -101,8 +101,13 @@ class DataSourceBase(util.MDTFObjectBase, util.CaseLoggerMixin):
         """
         yield from self.varlist.iter_vars()
 
-    def get_varlist(self, parent):
-        return varlist_util.Varlist.from_struct(parent)
+    def get_varlist(self, var_dict: dict):
+        return varlist_util.Varlist.from_struct(var_dict)
+
+    def translate_varlist(self, varlist, from_convention: str, to_convention: str):
+        fieldlist_from = get_fieldlist_table(from_convention)
+        fieldlist_to = get_fieldlist_table(to_convention)
+
 
     def query_dataset(self, var):
         """Find all rows of the catalog matching relevant attributes of the

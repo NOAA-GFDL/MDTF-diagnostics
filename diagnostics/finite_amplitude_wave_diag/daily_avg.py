12d41e89f815f1e67306f9b0b58b6f2f10deaa6b
csyhuang <csyhuang@uchicago.edu>
2023-08-18 21:25:03 -0500

    daily_avg

--- diagnostics/finite_amplitude_wave_diag/daily_avg.py
+++ diagnostics/finite_amplitude_wave_diag/daily_avg.py
@@ -8,16 +8,17 @@ import matplotlib.pyplot as plt    # python library we use to make plots
 u_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
 v_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
 t_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
-wk_dir = "/home/clare/GitHub/mdtf/wkdir"
+wk_dir = "/home/clare/GitHub/mdtf/wkdir/"
 
 
 u_file = xr.open_dataset(u_path)  # xarray.Dataset
 print(u_file)
 print("================")
-u_field = u_file['ua'].isel(time=slice(0, 500))
-print(u_field)
+u_daily_avg = u_file['ua'].groupby("time.day").mean(dim='time')
+print(u_daily_avg)
 print("================")
-ans = u_field.groupby("time.day").mean(dim='time')
-print(ans)
-print("================")
-print(f"ans.shape:\n{ans.shape}")
+print(f"u_daily_avg.shape:\n{u_daily_avg.shape}")
+output_file = f"{wk_dir}u_daily_avg.nc"
+print(f"Start outputing file: {output_file}")
+u_daily_avg.to_netcdf(f"{wk_dir}u_daily_avg.nc")
+print(f"Finished outputing file: {output_file}")

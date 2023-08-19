6e1d37ac52d0261948404920d8c775d7d19a845b
csyhuang <csyhuang@uchicago.edu>
2023-08-18 21:04:14 -0500

    try running daily_avg

---
+++ diagnostics/finite_amplitude_wave_diag/daily_avg.py
@@ -0,0 +1,16 @@
+"""
+Compute daily average from sample files
+"""
+import xarray as xr                # python library we use to read netcdf files
+import matplotlib.pyplot as plt    # python library we use to make plots
+
+
+u_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
+v_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
+t_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
+
+u_file = xr.open_dataset(u_path)  # xarray.Dataset
+u_field = u_file['ua']
+ans = u_field.groupby("time.month").mean(dim='time')
+print(ans)
+print(ans.shape)

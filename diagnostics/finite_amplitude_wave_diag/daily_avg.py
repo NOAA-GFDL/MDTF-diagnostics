acf74860c2ca11629e00c7526280f75733efc2eb
csyhuang <csyhuang@uchicago.edu>
2023-08-18 21:20:18 -0500

    daily_avg

--- diagnostics/finite_amplitude_wave_diag/daily_avg.py
+++ diagnostics/finite_amplitude_wave_diag/daily_avg.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt    # python library we use to make plots
 u_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
 v_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
 t_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
+wk_dir = "/home/clare/GitHub/mdtf/wkdir"
 
-u_file = xr.open_dataset(u_path)  # xarray.Dataset
+
+u_file = xr.load_dataset(u_path)  # xarray.Dataset
 print(u_file)
 print("================")
 u_field = u_file['ua'].isel(time=slice(0, 500))

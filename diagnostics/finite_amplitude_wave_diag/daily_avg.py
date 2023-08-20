8eb078033dc11c6432dd993e8ceaf5943ee4ff9d
csyhuang <csyhuang@uchicago.edu>
2023-08-19 20:39:23 -0500

    add var_names

--- diagnostics/finite_amplitude_wave_diag/daily_avg.py
+++ diagnostics/finite_amplitude_wave_diag/daily_avg.py
@@ -30,13 +30,20 @@ def output_daily_avg(input_path, output_file, varname="ua"):
 
 
 if __name__ == '__main__':
+    var_names = {
+        'plev': 'level',
+        'ylat': 'lat',
+        'xlon': 'lon',
+        'u': 'ua',
+        'v': 'va',
+        't': 'ta'}
     # u_output_path = output_daily_avg(u_path, "u_daily_mean.nc", varname="ua")
     # v_output_path = output_daily_avg(v_path, "v_daily_mean.nc", varname="va")
     # t_output_path = output_daily_avg(t_path, "t_daily_mean.nc", varname="ta")
     data_u = xr.load_dataset(u_daily_mean_path)
     data_v = xr.load_dataset(v_daily_mean_path)
     data_t = xr.load_dataset(t_daily_mean_path)
-    qgds = QGDataset(data_u, data_v, data_t)
+    qgds = QGDataset(data_u, data_v, data_t, var_names=var_names)
     uvtinterp = qgds.interpolate_fields()
     refstates = qgds.compute_reference_states()
 

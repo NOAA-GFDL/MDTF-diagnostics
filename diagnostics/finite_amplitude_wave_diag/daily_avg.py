109cd6563b2a5cd82a5e5009d99c9cfd02c97904
csyhuang <csyhuang@uchicago.edu>
2023-08-19 20:27:36 -0500

    add dependencies

--- diagnostics/finite_amplitude_wave_diag/daily_avg.py
+++ diagnostics/finite_amplitude_wave_diag/daily_avg.py
@@ -3,12 +3,15 @@ Compute daily average from sample files
 """
 import xarray as xr                # python library we use to read netcdf files
 import matplotlib.pyplot as plt    # python library we use to make plots
-
+from hn2016_falwa.xarrayinterface import QGDataset
 
 u_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
 v_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
 t_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
 wk_dir = "/home/clare/GitHub/mdtf/wkdir/"
+u_daily_mean_path = f"{wk_dir}u_daily_mean.nc"
+v_daily_mean_path = f"{wk_dir}v_daily_mean.nc"
+t_daily_mean_path = f"{wk_dir}t_daily_mean.nc"
 
 
 def output_daily_avg(input_path, output_file, varname="ua"):
@@ -28,6 +31,13 @@ def output_daily_avg(input_path, output_file, varname="ua"):
 
 if __name__ == '__main__':
     # u_output_path = output_daily_avg(u_path, "u_daily_mean.nc", varname="ua")
-    v_output_path = output_daily_avg(v_path, "v_daily_mean.nc", varname="va")
-    t_output_path = output_daily_avg(t_path, "t_daily_mean.nc", varname="ta")
+    # v_output_path = output_daily_avg(v_path, "v_daily_mean.nc", varname="va")
+    # t_output_path = output_daily_avg(t_path, "t_daily_mean.nc", varname="ta")
+    data_u = xr.load_dataset(u_daily_mean_path)
+    data_v = xr.load_dataset(v_daily_mean_path)
+    data_t = xr.load_dataset(t_daily_mean_path)
+    qgds = QGDataset(data_u, data_v, data_t)
+    uvtinterp = qgds.interpolate_fields()
+    refstates = qgds.compute_reference_states()
+
     print("Finished full procedures")

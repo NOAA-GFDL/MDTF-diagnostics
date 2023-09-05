957cf00e2327bea7ed39961a27caf74a04dc7887
csyhuang <csyhuang@uchicago.edu>
2023-09-05 18:47:46 -0500

    tried on model data, found unphysical issues

--- diagnostics/finite_amplitude_wave_diag/daily_avg_model_notfinished.py
+++ diagnostics/finite_amplitude_wave_diag/daily_avg_model_notfinished.py
@@ -1,14 +1,15 @@
 """
-Compute daily average from sample files
+Compute daily average from sample files. This is working on server
 """
+import os
 import xarray as xr                # python library we use to read netcdf files
 import matplotlib.pyplot as plt    # python library we use to make plots
 from hn2016_falwa.xarrayinterface import QGDataset
 
-u_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
-v_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
-t_path = "/home/clare/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
-wk_dir = "/home/clare/GitHub/mdtf/wkdir/"
+u_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ua.nc"
+v_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.va.nc"
+t_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.ta.nc"
+wk_dir = f"{os.environ['HOME']}/GitHub/mdtf/wkdir/"
 u_daily_mean_path = f"{wk_dir}u_daily_mean.nc"
 v_daily_mean_path = f"{wk_dir}v_daily_mean.nc"
 t_daily_mean_path = f"{wk_dir}t_daily_mean.nc"

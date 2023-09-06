b04b4bc29b035bdb78eea8bb4ddab2378f0fd8b1
csyhuang <csyhuang@uchicago.edu>
2023-09-05 20:44:02 -0500

    season then daily mean

--- diagnostics/finite_amplitude_wave_diag/era5_diag.py
+++ diagnostics/finite_amplitude_wave_diag/era5_diag.py
@@ -16,12 +16,13 @@ ylat_coord_name = os.environ["ylat_coord"]
 plev_coord_name = os.environ["plev_coord"]
 
 # 1) Loading model data files:
-data_u = xr.load_dataset(input_u_path)
-data_v = xr.load_dataset(input_v_path)
-data_t = xr.load_dataset(input_t_path)
+data_u = xr.load_dataset(input_u_path).sel(season='DJF').resample(time="1D").mean(dim="time")
+data_v = xr.load_dataset(input_v_path).sel(season='DJF').resample(time="1D").mean(dim="time")
+data_t = xr.load_dataset(input_t_path).sel(season='DJF').resample(time="1D").mean(dim="time")
 
 # 2) Doing computations:
 qgds = QGDataset(data_u, data_v, data_t, var_names={"u": u_var_name, "v": v_var_name, "t": t_var_name})
 uvtinterp = qgds.interpolate_fields()
 refstates = qgds.compute_reference_states()
 lwadiags = qgds.compute_lwa_and_barotropic_fluxes()
+print("Finished")

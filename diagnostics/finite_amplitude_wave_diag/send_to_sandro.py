fa30a820ad73ceb580779e422634654d4516bb0c
csyhuang <csyhuang@uchicago.edu>
2023-09-06 22:56:43 -0500

    update

--- diagnostics/finite_amplitude_wave_diag/send_to_sandro.py
+++ diagnostics/finite_amplitude_wave_diag/send_to_sandro.py
@@ -2,11 +2,12 @@
 Extract some sample data and send to Sandro
 """
 import os
+import numpy as np
 import xarray as xr                # python library we use to read netcdf files
 import matplotlib.pyplot as plt    # python library we use to make plots
 from hn2016_falwa.xarrayinterface import QGDataset
 
 original_file_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.[uvt]a.nc"
-file_handle = xr.open_mfdataset(original_file_path).resample(time="M").first()
+file_handle = xr.open_mfdataset(original_file_path).isel(time=np.arange(0, 8761, 730))
 print("file_handle")
 print(file_handle)

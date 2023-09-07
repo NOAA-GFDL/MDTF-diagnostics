01b8b2aa5ebba92dc8286ccadbd0dfc56e8ef412
csyhuang <csyhuang@uchicago.edu>
2023-09-06 22:57:09 -0500

    update

--- diagnostics/finite_amplitude_wave_diag/send_to_sandro.py
+++ diagnostics/finite_amplitude_wave_diag/send_to_sandro.py
@@ -8,6 +8,6 @@ import matplotlib.pyplot as plt    # python library we use to make plots
 from hn2016_falwa.xarrayinterface import QGDataset
 
 original_file_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.[uvt]a.nc"
-file_handle = xr.open_mfdataset(original_file_path).isel(time=np.arange(0, 8761, 730))
+file_handle = xr.open_mfdataset(original_file_path).isel(time=np.arange(0, 8760, 730))
 print("file_handle")
 print(file_handle)

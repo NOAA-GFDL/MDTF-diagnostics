fd280c99486c249530ebc69ebf8e46b06f6da97c
csyhuang <csyhuang@uchicago.edu>
2023-09-06 19:14:00 -0500

    remove unnecessary

--- diagnostics/finite_amplitude_wave_diag/otc_examine_model_data.py
+++ diagnostics/finite_amplitude_wave_diag/otc_examine_model_data.py
@@ -31,7 +31,7 @@ for tstep in np.arange(0, 1):
     zonal_mean_u = np.ma.masked_invalid(u_file.isel(time=tstep).ua.values).mean(axis=-1)
     zonal_mean_v = np.ma.masked_invalid(v_file.isel(time=tstep).va.values).mean(axis=-1)
     zonal_mean_t = np.ma.masked_invalid(t_file.isel(time=tstep).ta.values).mean(axis=-1)
-    time_str = str(u_file.isel(time=tstep)['time'].values)
+    time_str = str(u_file.isel(time=tstep)['time'].values).split()[0]
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 3))
     plt.suptitle(f"{time_str}")

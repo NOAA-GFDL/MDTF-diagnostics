a60d842280a6d62ba004b1d9f2cbd360da1dbe1e
csyhuang <csyhuang@uchicago.edu>
2023-09-06 22:53:44 -0500

    update

--- diagnostics/finite_amplitude_wave_diag/iMac_model_data_interpolate_before_gridfill.py
+++ diagnostics/finite_amplitude_wave_diag/iMac_model_data_interpolate_before_gridfill.py
@@ -1,5 +1,7 @@
 """
 Run this on OTC to compute reference state for 1 timestep
+Seems it is working on a coarse grid...
+Continue from here next time
 """
 import os
 import numpy as np
@@ -7,7 +9,7 @@ import xarray as xr                # python library we use to read netcdf files
 from diagnostics.finite_amplitude_wave_diag.gridfill_utils import gridfill_each_level
 from hn2016_falwa.xarrayinterface import QGDataset
 import matplotlib.pyplot as plt
-from hn2016_falwa.oopinterface import QGFieldNHN22
+from hn2016_falwa.oopinterface import QGFieldNHN22, QGFieldNH18
 
 
 wk_dir = f"{os.environ['HOME']}/Dropbox/GitHub/mdtf/wkdir/"
@@ -25,40 +27,46 @@ ylat = coord_file.coords['lat']
 plev = coord_file.coords['level']
 coord_file.close()
 
-all_file = xr.open_mfdataset("atmos_inst_t1000_[uvt].nc")
+run_poisson = False
+all_files = xr.open_mfdataset("atmos_inst_t1000_[uvt].nc")\
+    .interp(
+    coords={
+        "lat": np.arange(-90, 91, 1.5),
+        "lon": np.arange(0, 361, 1.5)},
+    method="linear",
+    kwargs={"fill_value": "extrapolate"})  # installed package bottleneck
+if not run_poisson:
+    all_files = all_files.fillna(10)
 gridfill_file = "atmos_inst_t1000_gridfill_{var}.nc"
 
 # *** First do poisson solver ***
-run_poisson = True
 if run_poisson:
     args_tuple = ['ua', 'va', 'ta']
     field_list = []
     for var_name in args_tuple:
         field_at_all_level = xr.apply_ufunc(
             gridfill_each_level,
-            *[all_file[var_name]],
+            *[all_files[var_name]],
             input_core_dims=(('lat', 'lon'),),
             output_core_dims=(('lat', 'lon'),),
             vectorize=True, dask="allowed")
         gridfill_file_path = gridfill_file.format(var=var_name)
         field_at_all_level.to_netcdf(gridfill_file_path)
         print(f"Finished outputing {gridfill_file_path}")
+    all_files = xr.open_mfdataset("atmos_inst_t1000_gridfill_[uvt]a.nc")
 
 # *** Interpolate onto regular grid ***
-all_files = xr.open_mfdataset("atmos_inst_t1000_gridfill_[uvt]a.nc")
-all_files = all_files.interp(
-    coords={
-        "lat": np.arange(-90, 91, 1.5),
-        "lon": np.arange(0, 361, 1.5)},
-    method="nearest",
-    kwargs={"fill_value": "extrapolate"})
-qgds = QGDataset(all_files, var_names={"u": "ua", "v": "va", "t": "ta"}, qgfield=QGFieldNHN22)
+qgds = QGDataset(
+    all_files,
+    var_names={"u": "ua", "v": "va", "t": "ta"},
+    qgfield=QGFieldNH18)
 uvtinterp = qgds.interpolate_fields()
 plt.contourf(
     uvtinterp['interpolated_u'].ylat,
     uvtinterp['interpolated_u'].height,
     uvtinterp['interpolated_u'].mean(axis=-1),
-    np.arange(-50, 51, 5))
+    np.arange(-50, 51, 5), cmap='rainbow')
+plt.title("Zonal mean zonal wind")
 plt.colorbar()
 plt.show()
 print("Finished interpolate_fields")
@@ -66,9 +74,44 @@ refstates = qgds.compute_reference_states()  # Error arises when solving referen
 plt.contourf(refstates['uref'].ylat,
              refstates['uref'].height,
              refstates['uref'],
-             np.arange(-50, 51, 5))
+             np.arange(-50, 101, 5), cmap='rainbow')
+plt.title("Uref")
 plt.colorbar()
 plt.show()
 print("Finished compute_reference_states")
+refstates = qgds.compute_lwa_and_barotropic_fluxes()  # Error arises when solving reference state
+plt.contourf(refstates['lwa'].ylat,
+             refstates['lwa'].height,
+             refstates['lwa'].mean(axis=-1), np.arange(0, 200, 10), cmap='rainbow')
+plt.title("Zonal mean FAWA")
+plt.colorbar()
+plt.show()
+print("Finished compute_lwa_and_barotropic_fluxes")
+
+plt.contourf(refstates['lwa_baro'].xlon,
+             refstates['lwa_baro'].ylat,
+             refstates['lwa_baro'],
+             40, cmap='rainbow')
+plt.title("Barotropic LWA")
+plt.colorbar()
+plt.show()
+
+height_level_index = 10
+
+plt.contourf(uvtinterp['qgpv'].xlon,
+             uvtinterp['qgpv'].ylat,
+             uvtinterp['qgpv'].isel(height=height_level_index),
+             40, cmap='rainbow')
+plt.title(f"QGPV at k={height_level_index}")
+plt.colorbar()
+plt.show()
+
+plt.contourf(refstates['lwa'].xlon,
+             refstates['lwa'].ylat,
+             refstates['lwa'].isel(height=height_level_index),
+             40, cmap='rainbow')
+plt.title(f"LWA at k={height_level_index}")
+plt.colorbar()
+plt.show()
 
 
---
+++ diagnostics/finite_amplitude_wave_diag/send_to_sandro.py
@@ -0,0 +1,12 @@
+"""
+Extract some sample data and send to Sandro
+"""
+import os
+import xarray as xr                # python library we use to read netcdf files
+import matplotlib.pyplot as plt    # python library we use to make plots
+from hn2016_falwa.xarrayinterface import QGDataset
+
+original_file_path = f"{os.environ['HOME']}/GitHub/mdtf/inputdata/model/GFDL-CM4/data/atmos_inst/ts/hourly/1yr/atmos_inst.1984010100-1984123123.[uvt]a.nc"
+file_handle = xr.open_mfdataset(original_file_path).resample(time="M").first()
+print("file_handle")
+print(file_handle)

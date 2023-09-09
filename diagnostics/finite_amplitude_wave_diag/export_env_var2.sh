2ebfe9a7b4385c1d528d1f2cd40ae012e23f3a1b
csyhuang <csyhuang@uchicago.edu>
2023-09-08 21:44:46 -0500

    fix names

--- diagnostics/finite_amplitude_wave_diag/export_env_var2.sh
+++ diagnostics/finite_amplitude_wave_diag/export_env_var2.sh
@@ -1,7 +1,7 @@
 export UVT_FILE=$HOME/GitHub/mdtf/inputdata/model/CMIP5/output1/NOAA-GFDL/GFDL-CM3/historical/day/atmos/day/r1i1p1/v20120227/*a/[uvt]a_day_GFDL-CM3_historical_r1i1p1_20050101-20051231.nc
-export U_VAR=u
-export V_VAR=v
-export T_VAR=t
+export U_VAR=ua
+export V_VAR=va
+export T_VAR=ta
 export TIME_COORD=time
 export LON_COORD=lon
 export LAT_COORD=lat

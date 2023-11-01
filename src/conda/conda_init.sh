627ad2525b9c67d1572a9516f959ad19fbb0f85b
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2023-11-01 17:36:36 -0400

    add conda binary check back to init script

--- src/conda/conda_init.sh
+++ src/conda/conda_init.sh
@@ -143,7 +143,9 @@ unset CONDA_SHLVL
 conda_check=$( $CONDA_EXE info )
 __conda_setup=""
 if [ -n "$conda_check" ]; then
+   if [[ "$CONDA_EXE" -eq "${_CONDA_ROOT}/bin/conda" || "$CONDA_EXE" -eq "${_CONDA_ROOT}/condabin/conda" ]]; then
       __conda_setup="$( $CONDA_EXE 'shell.bash' 'hook' 2> /dev/null )"
+   fi
 fi
 
 if [[ $? -eq 0 && -n "$__conda_setup" ]]; then
@@ -159,7 +161,6 @@ else
         alias micromamba="$CONDA_EXE"
     else
         export PATH="${_CONDA_ROOT}/bin:$PATH"
-
     fi
 fi
 unset __conda_setup

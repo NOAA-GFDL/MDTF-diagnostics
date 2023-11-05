9635fc49aafa561b3fc2315ed6e2ea07049170dc
csyhuang <csyhuang@uchicago.edu>
2023-11-04 21:53:13 -0500

    masked from second lowest level

--- diagnostics/finite_amplitude_wave_diag/finite_amplitude_wave_diag_zonal_mean.py
+++ diagnostics/finite_amplitude_wave_diag/finite_amplitude_wave_diag_zonal_mean.py
@@ -112,10 +112,10 @@ class DataPreprocessor:
             self._gridfill_needed = False
 
     def _do_save_mask(self, dataset):
-        self._yz_mask = dataset[self._u_var_name].isel({self._new_time_coord_name: 0})\
-            .to_masked_array().mask.sum(axis=-1).astype(bool)
-        self._xy_mask = dataset[self._u_var_name].isel({self._new_time_coord_name: 0})\
-            .to_masked_array().mask[:, :, :].sum(axis=0).astype(bool)
+        self._yz_mask = dataset[self._u_var_name]\
+            .to_masked_array().mask.sum(axis=0).sum(axis=-1).astype(bool)
+        self._xy_mask = dataset[self._u_var_name]\
+            .to_masked_array().mask[:, 1:, :, :].sum(axis=0).sum(axis=0).astype(bool)
 
     def _save_preprocessed_data(self, dataset, output_path):
         dataset.to_netcdf(output_path)

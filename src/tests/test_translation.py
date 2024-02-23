723a2e9163bcfdf150b09d38aed757c402489e09
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-02-23 16:04:47 -0500

    rename src/tests/test_core.py and start refactoring

--- src/tests/test_translation.py
+++ src/tests/test_translation.py
@@ -1,12 +1,7 @@
 import os
 import unittest
-from collections import namedtuple
-import itertools
-import unittest.mock as mock # define mock os.environ so we don't mess up real env vars
-import src.core as core
-# from src.data_manager import DataManager
-import src.diagnostic as diagnostic
 from src.tests.shared_test_utils import setUp_config_singletons, tearDown_config_singletons
+from src import translation, varlist_util, pod_setup, util
 
 
 class TestVariableTranslator(unittest.TestCase):
@@ -24,35 +19,37 @@ class TestVariableTranslator(unittest.TestCase):
         "PLACEHOLDER_X_COORD": {"axis": "X", "standard_name": "longitude", "units": "degrees_east"},
         "PLACEHOLDER_Y_COORD": {"axis": "Y", "standard_name": "latitude", "units": "degrees_north"},
         "PLACEHOLDER_Z_COORD": {
-        "standard_name": "air_pressure",
-        "units": "hPa",
-        "positive": "down",
-        "axis": "Z"
+            "standard_name": "air_pressure",
+            "units": "hPa",
+            "positive": "down",
+            "axis": "Z"
         },
         "PLACEHOLDER_T_COORD": {"axis": "T", "standard_name": "time", "units": "days"}
     }
 
     def test_variabletranslator(self):
-        temp = core.VariableTranslator()
+        temp = translation.VariableTranslator()
         temp.add_convention({
-            'name':'not_CF', 'coords': self._dummy_coords_d,
-            'variables':{
+            'name': 'not_CF', 'coords': self._dummy_coords_d,
+            'variables': {
                 'PRECT': {"standard_name": "pr_var", "units": "1", "ndim": 3},
                 'PRECC': {"standard_name": "prc_var", "units": "1", "ndim": 3}
             }
-        })
+        },
+        "")
         self.assertEqual(temp.to_CF_name('not_CF', 'PRECT'), 'pr_var')
         self.assertEqual(temp.from_CF_name('not_CF', 'pr_var'), 'PRECT')
 
     def test_variabletranslator_no_key(self):
-        temp = core.VariableTranslator()
+        temp = translation.VariableTranslator()
         temp.add_convention({
-            'name':'not_CF', 'coords': self._dummy_coords_d,
-            'variables':{
+            'name': 'not_CF', 'coords': self._dummy_coords_d,
+            'variables': {
                 'PRECT': {"standard_name": "pr_var", "units": "1", "ndim": 3},
                 'PRECC': {"standard_name": "prc_var", "units": "1", "ndim": 3}
             }
-        })
+        },
+        "")
         self.assertRaises(KeyError, temp.to_CF_name, 'B', 'PRECT')
         self.assertRaises(KeyError, temp.to_CF_name, 'not_CF', 'nonexistent_var')
         self.assertRaises(KeyError, temp.from_CF_name, 'B', 'PRECT')
@@ -60,18 +57,19 @@ class TestVariableTranslator(unittest.TestCase):
 
     def test_variabletranslator_aliases(self):
         # create multiple entries when multiple models specified
-        temp = core.VariableTranslator()
+        temp = translation.VariableTranslator()
         temp.add_convention({
-            'name':'not_CF', 'coords': self._dummy_coords_d,
+            'name': 'not_CF', 'coords': self._dummy_coords_d,
             'models': ['A', 'B'],
-            'variables':{
+            'variables': {
                 'PRECT': {"standard_name": "pr_var", "units": "1", "ndim": 3},
                 'PRECC': {"standard_name": "prc_var", "units": "1", "ndim": 3}
             }
-        })
+        },
+        "")
         self.assertEqual(temp.from_CF_name('not_CF', 'pr_var'), 'PRECT')
-        self.assertEqual(temp.from_CF_name('A','pr_var'), 'PRECT')
-        self.assertEqual(temp.from_CF_name('B','pr_var'), 'PRECT')
+        self.assertEqual(temp.from_CF_name('A', 'pr_var'), 'PRECT')
+        self.assertEqual(temp.from_CF_name('B', 'pr_var'), 'PRECT')
 
     def test_variabletranslator_no_translation(self):
         dummy_varlist = {
@@ -91,9 +89,9 @@ class TestVariableTranslator(unittest.TestCase):
                 }
             }
         }
-        varlist = diagnostic.Varlist.from_struct(dummy_varlist, parent=None)
+        varlist = varlist_util.Varlist.from_struct(dummy_varlist)
         ve = varlist.vars[0]
-        translate = core.VariableTranslator().get_convention('None')
+        translate = translation.VariableTranslator().get_convention('None')
         tve = translate.translate(ve)
         self.assertEqual(ve.name, tve.name)
         self.assertEqual(ve.standard_name, tve.standard_name)
@@ -142,12 +140,14 @@ class TestVariableTranslator(unittest.TestCase):
         # test that supported modifier atmos_height is correct
         raised = False
         try:
-            varlist = diagnostic.Varlist.from_struct(dummy_varlist_correct, parent=None)
-        except Exception:
+            varlist = varlist_util.Varlist.from_struct(dummy_varlist_correct)
+        except Exception as exc:
+            print(exc)
             raised = True
         self.assertFalse(raised)
         # test that incorrect modifier height throws an error
-        self.assertRaises(ValueError, diagnostic.Varlist.from_struct, dummy_varlist_wrong, parent=None)
+        self.assertRaises(ValueError, varlist_util.Varlist.from_struct, dummy_varlist_wrong, parent=None)
+
 
 class TestVariableTranslatorFiles(unittest.TestCase):
     def tearDown(self):
@@ -162,9 +162,10 @@ class TestVariableTranslatorFiles(unittest.TestCase):
         code_root = os.path.dirname(os.path.dirname(cwd))
         raised = False
         try:
-            translate = core.VariableTranslator(code_root, unittest=False)
+            translate = translation.VariableTranslator(code_root, unittest=False)
             translate.read_conventions(code_root, unittest=False)
-        except Exception:
+        except Exception as exc:
+            print(exc)
             raised = True
         self.assertFalse(raised)
         self.assertIn('CMIP', translate.conventions)
@@ -174,18 +175,20 @@ class TestVariableTranslatorFiles(unittest.TestCase):
         # run in non-unit-test mode to test loading of config files
         cwd = os.path.dirname(os.path.realpath(__file__))
         code_root = os.path.dirname(os.path.dirname(cwd))
-        translate = core.VariableTranslator(code_root, unittest=False)
+        translate = translation.VariableTranslator(code_root, unittest=False)
         translate.read_conventions(code_root, unittest=False)
         self.assertEqual(translate.to_CF_name('NCAR', 'PRECT'), "precipitation_rate")
-        self.assertEqual(translate.from_CF_name('CMIP', 'toa_outgoing_longwave_flux'), "rlut")
+        self.assertEqual(translate.from_CF_name('CMIP', 'toa_outgoing_longwave_flux'),
+                         "rlut")
+
 
 class TestPathManager(unittest.TestCase):
     # pylint: disable=maybe-no-member
     def setUp(self):
         # set up translation dictionary without calls to filesystem
-        setUp_config_singletons(paths = {
-            'CODE_ROOT':'A', 'OBS_DATA_ROOT':'B', 'MODEL_DATA_ROOT':'C',
-            'WORKING_DIR':'D', 'OUTPUT_DIR':'E'
+        setUp_config_singletons(paths={
+            'CODE_ROOT': 'A', 'OBS_DATA_ROOT': 'B', 'MODEL_DATA_ROOT': 'C',
+            'WORK_DIR': 'D', 'OUTPUT_DIR': 'E'
         })
 
     def tearDown(self):
@@ -194,41 +197,39 @@ class TestPathManager(unittest.TestCase):
     # ------------------------------------------------
 
     def test_pathmgr_global(self):
-        paths = core.PathManager()
+        paths = util.ModelDataPathManager()
         self.assertEqual(paths.CODE_ROOT, 'A')
         self.assertEqual(paths.OUTPUT_DIR, 'E')
 
     @unittest.skip("")
     def test_pathmgr_global_asserterror(self):
         d = {
-            'OBS_DATA_ROOT':'B', 'MODEL_DATA_ROOT':'C',
-            'WORKING_DIR':'D', 'OUTPUT_DIR':'E'
+            'OBS_DATA_ROOT': 'B', 'MODEL_DATA_ROOT': 'C',
+            'WORK_DIR': 'D', 'OUTPUT_DIR': 'E'
         }
-        paths = core.PathManager()
+        paths = util.ModelDtataPathManager()
         self.assertRaises(AssertionError, paths.parse, d, list(d.keys()))
-        # initialize successfully so that tear_down doesn't break
-        #_ = core.PathManager(unittest = True)
 
 
 @unittest.skip("TODO: Test needs to be rewritten following v3 beta 3 release")
-#@mock.patch.multiple(DataManager, __abstractmethods__=set())
+# @mock.patch.multiple(DataManager, __abstractmethods__=set())
 class TestPathManagerPodCase(unittest.TestCase):
     def setUp(self):
         # set up translation dictionary without calls to filesystem
         setUp_config_singletons(
             config=self.case_dict,
             paths={
-                'CODE_ROOT':'A', 'OBS_DATA_ROOT':'B', 'MODEL_DATA_ROOT':'C',
-                'WORKING_DIR':'D', 'OUTPUT_DIR':'E'
+                'CODE_ROOT': 'A', 'OBS_DATA_ROOT': 'B', 'MODEL_DATA_ROOT': 'C',
+                'WORK_DIR': 'D', 'OUTPUT_DIR': 'E'
             },
-            pods={ 'AA':{
-                'settings':{},
-                'varlist':[{'var_name': 'pr_var', 'freq':'mon'}]
-                }
+            pods={'AA': {
+                'settings': {},
+                'varlist': [{'var_name': 'pr_var', 'freq': 'mon'}]
+            }
             })
 
     case_dict = {
-        'CASENAME': 'A', 'model': 'B', 'FIRSTYR': 1900, 'LASTYR': 2100,
+        'CASENAME': 'A', 'model': 'B', 'startdate': 19000101, 'enddate': 21001231,
         'pod_list': ['AA']
     }
 
@@ -236,21 +237,20 @@ class TestPathManagerPodCase(unittest.TestCase):
         tearDown_config_singletons()
 
     def test_pathmgr_model(self):
-        paths = core.PathManager()
+        paths = util.ModelDataPathManager()
         case = DataManager(self.case_dict)
         d = paths.model_paths(case)
         self.assertEqual(d['MODEL_DATA_DIR'], 'TEST_MODEL_DATA_ROOT/A')
         self.assertEqual(d['MODEL_WK_DIR'], 'TEST_WORKING_DIR/MDTF_A_1900_2100')
 
     def test_pathmgr_pod(self):
-        paths = core.PathManager()
+        paths = util.ModelPathManager()
         case = DataManager(self.case_dict)
-        pod = diagnostic.Diagnostic('AA')
+        pod = pod_setup.PodObject('AA', ctx_config)
         d = paths.pod_paths(pod, case)
         self.assertEqual(d['POD_CODE_DIR'], 'TEST_CODE_ROOT/diagnostics/AA')
         self.assertEqual(d['POD_OBS_DATA'], 'TEST_OBS_DATA_ROOT/AA')
         self.assertEqual(d['POD_WK_DIR'], 'TEST_WORKING_DIR/MDTF_A_1900_2100/AA')
-
 
 
 # ---------------------------------------------------

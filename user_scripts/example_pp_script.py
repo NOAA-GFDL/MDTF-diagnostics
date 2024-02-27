5b0de3419e1e5deb4716689a878e14a768dfdee8
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-02-27 15:55:33 -0500

    create an example custom preprocessing script

---
+++ user_scripts/example_pp_script.py
@@ -0,0 +1,174 @@
+"""Example custom preprocessing script to include in framework workflow"""
+# Import modules used in the POD
+import os
+import sys
+import collections
+import numpy as np
+import pandas as pd
+import xarray as xr
+import logging
+from src import util, cli
+
+# Define a log object for debugging
+_log = logging.getLogger(__name__)
+
+
+def check_group_daterange(group_df: pd.DataFrame, log=_log) -> pd.DataFrame:
+    """Sort the files found for each experiment by date, verify that
+    the date ranges contained in the files are contiguous in time and that
+    the date range of the files spans the query date range.
+
+    Args:
+        group_df (Pandas Dataframe):
+        log: log file
+    """
+    date_col = "date_range"
+    try:
+        # method throws ValueError if ranges aren't contiguous
+        dates_df = group_df.loc[:, ['start_time', 'end_time']]
+        date_range_vals = []
+        for idx, x in enumerate(group_df.values):
+            st = dates_df.at[idx, 'start_time']
+            en = dates_df.at[idx, 'end_time']
+            date_range_vals.append(util.DateRange(st, en))
+
+        group_df = group_df.assign(date_range=date_range_vals)
+        sorted_df = group_df.sort_values(by=date_col)
+
+        # throws ValueError if we don't span the query range
+        return sorted_df
+    except ValueError:
+        log.error("Non-contiguous or malformed date range in files:", sorted_df["path"].values)
+    return pd.DataFrame(columns=group_df.columns)
+
+
+def rename_dataset_keys(ds: dict, case_list: dict) -> collections.OrderedDict:
+    """Rename dataset keys output by ESM intake catalog query to case names`"""
+
+    def rename_key(old_dict: dict, new_dict: collections.OrderedDict, old_key, new_key):
+        """Credit:  https://stackoverflow.com/questions/16475384/rename-a-dictionary-key"""
+        new_dict[new_key] = old_dict[old_key]
+
+    new_dict = collections.OrderedDict()
+    case_names = [c for c in case_list.keys()]
+    for old_key, case_d in ds.items():
+        (path, filename) = os.path.split(case_d.attrs['intake_esm_attrs:path'])
+        rename_key(ds, new_dict, old_key, [c for c in case_names if c in filename][0])
+    return new_dict
+
+
+# Main script that works on the xarray dataset that the framework reads from the input data catalog
+# Functions adapted from albedofb_calcs.py
+
+def main(xr_ds: xr.Dataset) -> xr.Dataset:
+    # 1. Reshape the data array to convert dimensions to sub-dimensions defined by "coords" and "new_dims"
+    # define coordinate and new_dims arrays
+    ny = int(xr_ds['time'].sizes['time'] / 12)
+    coords = [np.arange(ny), np.arange(12)]
+    new_dims = ['year', 'month']
+
+    # Create a pandas MultiIndex
+    ind = pd.MultiIndex.from_product(coords, names=new_dims)
+
+    # Replace the time index in the DataArray by this new index
+    xr_ds.coords['time'] = ind
+
+    # Convert multi-index to individual dims using DataArray.unstack().
+    # This changes dimension order! The new dimensions are at the end.
+    xr_ds.unstack('time')
+
+    # Permute to restore dimensions
+    i = xr_ds.dims.index('time')
+    dims = list(xr_ds.dims)
+
+    for d in new_dims[::-1]:
+        dims.insert(i, d)
+
+    for d in new_dims:
+        _ = dims.pop(-1)
+
+    xr_ds.transpose(*dims)
+
+    # 2. compute the time mean
+    return xr_ds.mean(dim='year')
+
+
+# Anything in this block executes if the script is run on its own
+# > python3 example_pp_script.py
+# Test reading a catalog into an xarray dataset and passing it to you preprocessing routine
+# by modifying code block below
+
+
+if __name__ == '__main__':
+    import intake
+
+    # root directory of this script
+    code_root = os.path.dirname(os.path.realpath(__file__))
+
+    # full path to the runtime configuration file
+    # config_file = "[path to configuration file]/runtime_config.jsons"
+    config_file = "/Users/jess/mdtf/MDTF-diagnostics/templates/runtime_config.jsonc"
+
+    # read the contents of the configuration file into a NameSpace (basically a dict with dot notation)
+    config = cli.parse_config_file(config_file)
+
+    # full path to the input data catalog json file
+    data_catalog = "[path_to_catalog]/catalog_name.json"
+
+    # open the csv file using information provided by the catalog definition file
+    cat = intake.open_esm_datastore(data_catalog)
+
+    # dictionary to hold the data subset returned by the catalog query
+    cat_dict = {}
+
+    # create filter lists for POD variables
+    cols = list(cat.df.columns.values)
+
+    # Add a date_range column to the catalog dictionary if necessary
+    if 'date_range' not in [c.lower() for c in cols]:
+        cols.append('date_range')
+
+    # define a variable dictionary with the name, standard_name, realm, output frequency, and any other attributes
+    # you want to use in the catalog query
+    # note that this example uses monthly data
+    var_list = {"tas":
+                {
+                    "standard_name": "air_temperature",
+                    "freq": "mon",
+                    "realm": "atmos"
+                }
+    }
+
+    # loop through the case list and read in the desired files
+    for case_name, case_d in config.case_list.items():
+        path_regex = case_name + '*'  # use wild cards to find the appropriate case
+
+        # loop through the variables in the dictionary
+        for k, v in var_list.items():
+            cat_subset = cat.search(activity_id=case_d.convention,
+                                    standard_name=v['standard_name'],
+                                    frequency=v['freq'],
+                                    realm=v['realm'],
+                                    path=path_regex
+                                    )
+            if cat_subset.df.empty:
+                raise util.DataRequestError(f"No assets found for {case_name} in {data_catalog}")
+
+            # Get files in specified date range
+            cat_subset.esmcat._df = check_group_daterange(cat_subset.df)
+
+            # convert subset catalog to an xarray dataset dict
+            # and concatenate the result with the final dict
+            cat_dict = cat_dict | cat_subset.to_dataset_dict(
+                xarray_open_kwargs={"decode_times": True, "use_cftime": True}
+            )
+
+    # rename cat_subset case dict keys to case names
+    new_cat = rename_dataset_keys(cat_dict, config.case_list)
+
+    # run the main routine on the xarray dataset
+    for case_name, case_xr_dataset in new_cat.items():
+        for v in var_list.values():
+            xr_ds_new = main(case_xr_dataset, v)
+
+    sys.exit(0)

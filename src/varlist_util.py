958fd3e6c31dfea51c1a608f97904ec0192b981f
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-07-08 17:13:42 -0400

    add varlist_util to commit

--- src/varlist_util.py
+++ src/varlist_util.py
@@ -334,10 +334,15 @@ class VarlistEntry(VarlistEntryBase, util.MDTFObjectBase, data_model.DMVariable,
         })
 
         for ax, dim in self.dim_axes.items():
-            trans_dim = self.translation.axes[ax]
-            self.env_vars[dim.name + _coord_env_var_suffix] = trans_dim.name
-            if trans_dim.has_bounds:
-                self.env_vars[dim.name + _coord_bounds_env_var_suffix] = trans_dim.bounds
+            if self.translation is not None:
+                trans_dim = self.translation.axes[ax]
+                self.env_vars[dim.name + _coord_env_var_suffix] = trans_dim.name
+                if trans_dim.has_bounds:
+                    self.env_vars[dim.name + _coord_bounds_env_var_suffix] = trans_dim.bounds
+            else:
+                self.env_vars[dim.name + _coord_env_var_suffix] = dim.name
+                if dim.has_bounds:
+                    self.env_vars[dim.name + _coord_bounds_env_var_suffix] = dim.bounds
 
     def iter_alternates(self):
         """Breadth-first traversal of "sets" of alternate VarlistEntries,
@@ -569,8 +574,30 @@ class Varlist(data_model.DMDataSet):
             # store but don't deactivate, because preprocessor.edit_request()
             # may supply alternate variables
             v.log.store_exception(chained_exc)
-        # set the VarlistEntry env_vars (required for backwards compatibility with first-gen PODs)
+
+        # set the VarlistEntry env_vars (required for backwards compatibility with first-gen PODs
         v.set_env_vars()
+        # Translate alternate vars if necessary
+        for alt_v in v.alternates:
+            if alt_v.T is not None:
+                alt_v.change_coord(
+                    'T',
+                    new_class={
+                        'self': VarlistTimeCoordinate,
+                        'range': util.DateRange,
+                        'frequency': util.DateFrequency
+                    },
+                    range=date_range,
+                    calendar=util.NOTSET,
+                    units=util.NOTSET
+                )
+            alt_v.dest_path = self.variable_dest_path(model_paths, case_name, alt_v)
+            trans_alt = translate.translate(alt_v, from_convention)
+            alt_v.translation = trans_alt
+            if trans_alt is None:
+                alt_v.info(f'Note: alternate variable {alt_v.full_name} not translated from {from_convention} to'
+                           f' {to_convention}')
+            alt_v.set_env_vars()
 
     def variable_dest_path(self,
                            model_paths: util.ModelDataPathManager,

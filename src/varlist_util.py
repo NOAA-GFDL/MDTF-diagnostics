dcb3f110492bc1dfd6526a3459cd83507252c07f
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-07-26 17:03:02 -0400

    fix alternates configuration in varlist_util.from_struct so that alternates are only populted from current pod object info when running on multiple pods

--- src/varlist_util.py
+++ src/varlist_util.py
@@ -664,15 +664,22 @@ class Varlist(data_model.DMDataSet):
             for k, v in parent.pod_vars.items()}
         )
 
-        alt_vars = [k for k, v in cls.vlist_vars.items() if v.is_alternate]
-        for v in cls.vlist_vars.values():
+        alt_vars = []
+        for k, v in parent.pod_vars.items():
+            for vv in v.values():
+                if vv == 'alternate':
+                    alt_vars.append(k)
+        vvars = tuple(parent.pod_vars.keys())
+        print(vvars)
+        for k, v in parent.pod_vars.items():
             # validate & replace names of alt vars with references to VE objects
-            for altv_name in v.alternates:
-                if altv_name not in cls.vlist_vars:
-                    raise ValueError((f"Unknown variable name {altv_name} listed "
-                                      f"in alternates for varlist entry {v.name}."))
-            linked_alts = [cls.vlist_vars[v_name] for v_name in alt_vars]
-            v.alternates = linked_alts
+            if v.get('alternates', None) is not None:
+                for altv_name in v.get('alternates'):
+                    if altv_name not in vvars:
+                        raise ValueError((f"Unknown variable name {altv_name} listed "
+                                          f"in alternates for varlist entry {v.name}."))
+                linked_alts = [cls.vlist_vars[v_name] for v_name in alt_vars]
+                cls.vlist_vars[k].alternates = linked_alts
         for a in alt_vars:
             cls.vlist_vars = util.new_dict_wo_key(cls.vlist_vars, a)
 

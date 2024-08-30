58917a7cc8826e7de6cc5ffaa4182c5be10da40b
Jess <20195932+wrongkindofdoctor@users.noreply.github.com>
2024-08-30 11:24:16 -0400

    Ci fixes (#675)
    * remove backslashes from dataclass docstrings
    
    * clean up datelabel docstrings
    
    * add name and standard_name attributes to xr variables returned by catalog query if they are not defined in the file metadata
    
    * modify xr_parser reconcile_names to add check for variable case sensitivity, check variable attributes for the standard_name
    instead of assuming that it is already present

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -338,7 +338,7 @@ class RenameVariablesFunction(PreprocessorFunctionBase):
             except:
                 pass
         [rename_d.pop(t) for t in translated]
-    
+
         return ds.rename(rename_d)
 
 
@@ -763,13 +763,13 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
 
         # only check that up to monthly precision for monthly or longer data
         if freq in ['mon', 'year']:
-            if t_start.year > dt_start_upper.year or\
+            if t_start.year > dt_start_upper.year or \
                     t_start.year == dt_start_upper.year and t_start.month > dt_start_upper.month:
                 err_str = (f"Error: dataset start ({t_start}) is after "
                            f"requested date range start ({dt_start_upper}).")
                 var.log.error(err_str)
                 raise IndexError(err_str)
-            if t_end.year < dt_end_lower.year or\
+            if t_end.year < dt_end_lower.year or \
                     t_end.year == dt_end_lower.year and t_end.month < dt_end_lower.month:
                 err_str = (f"Error: dataset end ({t_end}) is before "
                            f"requested date range end ({dt_end_lower}).")
@@ -792,14 +792,14 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
         check_group_daterange and convert them into str type.
         This function also handles missing leading zeros
         """
-        poss_digits = [6,8,10,12,14]
+        poss_digits = list(range(4, 15, 2))
         for i in range(len(time_vals)):
             if isinstance(time_vals[i], str):
-                time_vals[i] = time_vals[i].replace('-', '').replace(':', '')
+                time_vals[i] = time_vals[i].replace(' ', '').replace('-', '').replace(':', '')
                 while len(time_vals[i]) not in poss_digits:
                     time_vals[i] = '0' + time_vals[i]
         return time_vals
-            
+
     def check_group_daterange(self, group_df: pd.DataFrame, case_dr,
                               log=_log) -> pd.DataFrame:
         """Sort the files found for each experiment by date, verify that
@@ -859,7 +859,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
             for i in sorted_df.index:
                 cat_row = sorted_df.iloc[i]
                 if pd.isnull(cat_row['start_time']):
-                    continue 
+                    continue
                 else:
                     st = dl.dt_to_str(cat_row['start_time'])
                     et = dl.dt_to_str(cat_row['end_time'])
@@ -867,7 +867,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                     etin = dl.Date(et) in case_dr
                 if stin and etin:
                     return_df.append(cat_row.to_dict())
-            
+
             return pd.DataFrame.from_dict(return_df)
         except ValueError:
             log.error("Non-contiguous or malformed date range in files:", group_df["path"].values)
@@ -917,7 +917,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
             # path_regex = re.compile(r'(?i)(?<!\\S){}(?!\\S+)'.format(case_name))
             path_regex = re.compile(r'({})'.format(case_name))
             # path_regex = '*' + case_name + '*'
-            
+
             for var in case_d.varlist.iter_vars():
                 realm_regex = var.realm + '*'
                 date_range = var.translation.T.range
@@ -930,11 +930,11 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                 case_d.query['path'] = [path_regex]
                 case_d.query['realm'] = realm_regex
                 case_d.query['standard_name'] = var.translation.standard_name
-                
+
                 # change realm key name if necessary
                 if cat.df.get('modeling_realm', None) is not None:
                     case_d.query['modeling_realm'] = case_d.query.pop('realm')
-               
+
                 # search catalog for convention specific query object
                 var.log.info("Querying %s for variable %s for case %s.",
                              data_catalog,
@@ -996,7 +996,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                 # easily be problematic in the future.
                 # tl;dr hic sunt dracones
                 time_sort_dict = {f: cat_subset_df[f].time.values[0]
-                                   for f in list(cat_subset_df)}
+                                  for f in list(cat_subset_df)}
                 time_sort_dict = dict(sorted(time_sort_dict.items(), key=lambda item: item[1]))
                 var_xr = []
                 for k in list(time_sort_dict):
@@ -1007,6 +1007,12 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                 for att in drop_atts:
                     if var_xr.get(att, None) is not None:
                         var_xr = var_xr.drop_vars(att)
+                # add standard_name to the variable xarray dataset if it is not defined
+                for vname in var_xr.variables:
+                    if (not isinstance(var_xr.variables[vname], xr.IndexVariable)
+                            and var_xr[vname].attrs.get('standard_name', None) is None):
+                        var_xr[vname].attrs['standard_name'] = case_d.query.get('standard_name')
+                        var_xr[vname].attrs['name'] = vname
                 if case_name not in cat_dict:
                     cat_dict[case_name] = var_xr
                 else:
@@ -1045,7 +1051,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                 # Call function with the arguments
                 # user_scripts.example_pp_script.main(xarray_ds, v)
                 xarray_ds = user_module.main(xarray_ds, v.name)
-        
+
         return xarray_ds
 
     def setup(self, pod):
@@ -1095,7 +1101,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
             (path, filename) = os.path.split(case_d.attrs['intake_esm_attrs:path'])
             rename_key(ds, new_dict, old_key, [c for c in case_names if c in filename][0])
         return new_dict
-    
+
     def rename_dataset_vars(self, ds: dict, case_list: dict) -> collections.OrderedDict:
         """Rename variables in dataset to conform with variable names requested by the POD"""
         case_names = [c for c in case_list.keys()]
@@ -1105,7 +1111,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
                 name_dict[var.translation.name] = var.name
             ds[c] = ds[c].rename_vars(name_dict=name_dict)
         return ds
-    
+
     def clean_nc_var_encoding(self, var, name, ds_obj):
         """Clean up the ``attrs`` and ``encoding`` dicts of *ds_obj*
         prior to writing to a netCDF file, as a workaround for the following

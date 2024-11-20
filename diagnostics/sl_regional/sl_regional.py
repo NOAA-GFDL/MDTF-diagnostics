c4915704756814bd55a46058ceecbf48e5424dbf
Nishchitha Etige <sandeep.drck@gmail.com>
2024-11-20 11:54:16 -0700

    pushing the sl_reginoal POD folder

---
+++ diagnostics/sl_regional/sl_regional.py
@@ -0,0 +1,8 @@
+import os
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs  # Assuming ccrs is Cartopy's Coordinate Reference Systems
+import xesmf as xe
+import numpy as np
+
+print("Libs imported!")

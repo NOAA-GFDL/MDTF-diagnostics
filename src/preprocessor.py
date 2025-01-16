715bbf23c24c3078ff8a45c5f06af7a37e44c71a
Jacob Mims <122570226+jtmims@users.noreply.github.com>
2025-01-15 22:12:03 -0500

    re-add whitespace

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -918,6 +918,7 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
             ds_end = self.cast_to_cftime(ds_end_time, cal)
         date_range_cf_start = self.cast_to_cftime(case_date_range.start.lower, cal)
         date_range_cf_end = self.cast_to_cftime(case_date_range.end.lower, cal)
+
         if ds_start < date_range_cf_start and ds_end < date_range_cf_start or \
            ds_end > date_range_cf_end and ds_start > date_range_cf_end:
             new_xr_ds = None
@@ -933,7 +934,8 @@ class MDTFPreprocessorBase(metaclass=util.MDTFABCMeta):
             new_xr_ds = xr_ds.sel({time_coord.name: slice(ds_start, date_range_cf_end)})
         # dataset contains all of requested date range
         elif date_range_cf_start>=ds_start and date_range_cf_end<=ds_end:
-            new_xr_ds = xr_ds.sel({time_coord.name: slice(date_range_cf_start, date_range_cf_end)})
+            new_xr_ds = xr_ds.sel({time_coord.name: slice(date_range_cf_start, date_range_cf_end)}
+)
         return new_xr_ds
 
     def check_group_daterange(self, df: pd.DataFrame, date_range: util.DateRange,

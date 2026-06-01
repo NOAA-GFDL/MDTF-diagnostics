a5db352819c43c5cfae23b9b46251343d8ae4718
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2026-06-01 17:25:39 -0400

    Delete data directory

--- data/gfdl-cmor-tables/pandas_usage.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-#JUST TO QUICKLY BE ABLE TO QUERY THE CSV FILE
-df = pd.read_csv('/home/a1r/cure/gfdl_to_cmip5_vars.csv',sep=",", header=0,index_col=False)
-
-print("All atmos fields and their GFDL, CMIP mappings")
-print(df[(df.modeling_realm == 'atmos')])
-
-
